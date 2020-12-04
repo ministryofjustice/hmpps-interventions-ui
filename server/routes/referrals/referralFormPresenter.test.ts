bbf8c18d1d5b3680dd6dbe828cc1658621213a76
Lawrence Forooghian <lawrence@dxw.com>
2020-12-04 10:50:53 +0000

    Display a task list on the referral form
    This displays a task list which currently shows a single fixed state
    - the user has half filled the referral form, and the next thing for
    them to do is to fill in the desired completion date for the
    accommodation intervention.
    
    The markup and styling here is taken from the corresponding screen in
    the prototype.
    
    Architecturally, here I’ve started to think about the different
    responsibilities that exist within the app. I’ve introduced the concept
    of a presenter, which is responsible for converting model data into
    structured data that’s ready to be directly presented by a view. For
    example, in the case of the referral form, it would be responsible for
    populating the task list sections, and deciding each of their statuses
    and enabled-ness based on the data in the referral. It’d also be
    reponsible for things like formatting model data.
    
    The main aim of the presenter is to keep the view templates as simple as
    possible, so that they're basically just looping over things and
    rendering pre-formatted strings. For example, even though none of the
    business logic for driving the referral form has yet been implemented,
    the view template will not need to change at all as we start to
    implement that logic - it’ll just be a change to logic in the presenter.
    
    The presenter is easier to test than a view template, too.
    
    On a practical note (separation of responibilities aside), we found in
    the prototypes that writing any kind of logic in the view templates is a
    deeply unpleasant experience, because Nunjucks is a rather difficult
    language to work with compared to e.g. TypeScript.
    
    I’ve also introduced the concept of a “view”, which is a class that
    bundles up a Nunjucks template name and some helper functions, and can
    be passed directly to a `render` call in the controller. Not sure how
    useful this one will be, let’s see.

---
+++ server/routes/referrals/referralFormPresenter.test.ts
@@ -0,0 +1,94 @@
+import ReferralFormPresenter, { ReferralFormStatus } from './referralFormPresenter'
+
+describe('ReferralFormPresenter', () => {
+  describe('sections', () => {
+    // This is not a very exciting test at the moment but will become more useful
+    // as the task list starts to handle different referral states.
+
+    it('returns an array of section presenters', () => {
+      const presenter = new ReferralFormPresenter({ id: '1' })
+
+      const expected = [
+        {
+          type: 'single',
+          title: 'Retrieve service user record',
+          number: '1',
+          status: ReferralFormStatus.Completed,
+          tasks: [
+            { title: 'Enter service user case identifier', url: '#' },
+            { title: 'Confirm service user details', url: '#' },
+          ],
+        },
+        {
+          type: 'single',
+          title: 'Find and select service user interventions',
+          number: '2',
+          status: ReferralFormStatus.Completed,
+          tasks: [
+            { title: 'Find and select service user interventions', url: '#' },
+            { title: 'Confirm interventions', url: '#' },
+          ],
+        },
+        {
+          type: 'single',
+          title: 'Review service user’s information',
+          number: '3',
+          status: ReferralFormStatus.Completed,
+          tasks: [
+            { title: 'Service user’s risk information', url: '#' },
+            { title: 'Service user’s needs and requirements', url: '#' },
+          ],
+        },
+        {
+          type: 'multi',
+          title: 'Add intervention referrals detail',
+          number: '4',
+          taskListSections: [
+            {
+              title: 'Accommodation referral',
+              number: '4.1',
+              status: ReferralFormStatus.InProgress,
+              tasks: [
+                { title: 'Select the relevant sentence for the accommodation referral', url: '#' },
+                { title: 'Select desired outcomes', url: '#' },
+                { title: 'Select required complexity level', url: '#' },
+                { title: 'What date does the accommodation service need to be completed by?', url: '#' },
+                { title: 'Enter RAR days used', url: null },
+                { title: 'Further information for service provider', url: null },
+              ],
+            },
+            {
+              title: 'Social inclusion referral',
+              number: '4.2',
+              status: ReferralFormStatus.NotStarted,
+              tasks: [
+                { title: 'Select the relevant sentence for the social inclusion referral', url: '#' },
+                { title: 'Select desired outcomes', url: null },
+                { title: 'Select required complexity level', url: null },
+                { title: 'What date does the social inclusion service need to be completed by?', url: null },
+                { title: 'Enter RAR days used', url: null },
+                { title: 'Further information for service provider', url: null },
+              ],
+            },
+          ],
+        },
+        {
+          type: 'single',
+          title: 'Review responsible officer’s information',
+          number: '5',
+          status: ReferralFormStatus.NotStarted,
+          tasks: [{ title: 'Responsible officer information', url: '#' }],
+        },
+        {
+          type: 'single',
+          title: 'Check your answers',
+          number: '6',
+          status: ReferralFormStatus.CannotStartYet,
+          tasks: [{ title: 'Check your answers', url: null }],
+        },
+      ]
+
+      expect(presenter.sections).toEqual(expected)
+    })
+  })
+})
