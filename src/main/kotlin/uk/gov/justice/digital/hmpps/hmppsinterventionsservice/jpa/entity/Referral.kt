6c771e0939d1333d3243ea62abe0f9d08dc6434c
Tom Myers <tom.myers@digital.justice.gov.uk>
2020-12-08 11:55:57 +0000

    change 'completionDeadline' field to postgres 'date' type. add sample data for local deploy

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/jpa/entity/Referral.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/jpa/entity/Referral.kt
@@ -1,6 +1,7 @@
 package uk.gov.justice.digital.hmpps.hmppsinterventionsservice.jpa.entity
 
 import org.hibernate.annotations.CreationTimestamp
+import java.time.LocalDate
 import java.time.OffsetDateTime
 import java.util.UUID
 import javax.persistence.Entity
@@ -9,7 +10,7 @@ import javax.persistence.Id
 
 @Entity
 data class Referral(
-  var completionDeadline: OffsetDateTime? = null,
+  var completionDeadline: LocalDate? = null,
   @CreationTimestamp var created: OffsetDateTime? = null,
   @Id @GeneratedValue var id: UUID? = null,
 )
---
+++ src/main/resources/db/local/V99_0__draft_referrals.sql
@@ -0,0 +1,4 @@
+insert into referral (id, created, completion_deadline)
+values ('F7B4A237-94CC-448F-A2C0-CDFFF17B58B9', '2020-12-07 18:02:01.599803+00', '2021-02-14'),
+       ('71E6EAF6-5899-4A6B-9651-1AA14B3843A3', '2020-12-07 20:45:21.986389+00', '2021-03-01');
+
