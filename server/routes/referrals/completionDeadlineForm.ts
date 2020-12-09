24db5f2ccbc318f323c8450c5a51edfe0e912038
Lawrence Forooghian <lawrence@dxw.com>
2020-12-09 10:02:55 +0000

    Create form class to handle deadline input
    The idea of this form class is that it handles all of the user input,
    including:
    
    - validation
    - sanitization
    - returning a list of parameters that can be directly passed to the
      InterventionsService#patchDraftReferral method
    
    The express-validator recommended way of adding validation is to do it
    in a middleware, but with our current setup that would have meant having
    to add it in server/routes/index.ts, which would have quickly got out of
    hand. I like having it contained in a class. I’m not thrilled about the
    fact that this mutates the request, but will live with it for now.
    
    I don’t know whether we’ll end up with one form class per screen, or
    (more likely, I think) a configurable form class that can handle any
    kind of update to the draft referral.
    
    There’s some work remaining to figure out how to meet the error handling
    requirements of the GOV.UK Design System, which specifies quite a
    granular level of error handling for date inputs. I decided not to get
    into that here because it seemed like an absolute rabbit hole.
    
    Tom suggested that we add the error messages into a separate file so
    that they could be easily reviewed by designers.
    
    Co-authored-by: George Eaton <george@dxw.com>

---
+++ server/routes/referrals/completionDeadlineForm.ts
@@ -0,0 +1,87 @@
+import { Request } from 'express'
+import { body, Result, SanitizationChain, ValidationError, validationResult } from 'express-validator'
+import { DraftReferral } from '../../services/interventionsService'
+import CalendarDay from '../../utils/calendarDay'
+import errorMessages from '../../utils/errorMessages'
+
+export default class CompletionDeadlineForm {
+  private constructor(private readonly request: Request, private readonly result: Result<ValidationError>) {
+    this.result = validationResult(request)
+  }
+
+  static async createForm(request: Request): Promise<CompletionDeadlineForm> {
+    await Promise.all(this.validations.map(validation => validation.run(request)))
+
+    const result = validationResult(request)
+
+    return new CompletionDeadlineForm(request, result)
+  }
+
+  static get validations(): SanitizationChain[] {
+    return [
+      // TODO decide whether we like the fact that express-validator is mutating the request?
+      // e.g. the `bail` before toInt() is so that we don’t end up replaying NaN back to the user
+      // if they enter a blank input
+      body('completion-deadline-day')
+        .notEmpty({ ignore_whitespace: true })
+        .withMessage(errorMessages.completionDeadline.dayEmpty)
+        .isInt()
+        .withMessage(errorMessages.completionDeadline.invalidDate)
+        .bail()
+        .toInt(),
+      body('completion-deadline-month')
+        .notEmpty({ ignore_whitespace: true })
+        .withMessage(errorMessages.completionDeadline.monthEmpty)
+        .isInt()
+        .withMessage(errorMessages.completionDeadline.invalidDate)
+        .bail()
+        .toInt(),
+      body('completion-deadline-year')
+        .notEmpty({ ignore_whitespace: true })
+        .withMessage(errorMessages.completionDeadline.yearEmpty)
+        .isInt()
+        .withMessage(errorMessages.completionDeadline.invalidDate)
+        .bail()
+        .toInt(),
+    ]
+  }
+
+  get paramsForUpdate(): Partial<DraftReferral> {
+    return {
+      completionDeadline: this.completionDeadline.iso8601,
+    }
+  }
+
+  get isValid(): boolean {
+    return this.errors === null
+  }
+
+  get errors(): CompletionDeadlineErrors | null {
+    if (this.result.isEmpty() && this.completionDeadline) {
+      return null
+    }
+
+    // TODO (IC-706) use `result`, implement all the rules of the GOV.UK Design
+    // System date input component error handling
+    return {
+      firstErroredField: 'day',
+      erroredFields: ['day', 'month', 'year'],
+      message: errorMessages.completionDeadline.invalidDate,
+    }
+  }
+
+  private get completionDeadline(): CalendarDay | null {
+    return CalendarDay.fromComponents(
+      this.request.body['completion-deadline-day'],
+      this.request.body['completion-deadline-month'],
+      this.request.body['completion-deadline-year']
+    )
+  }
+}
+
+// This interface is up for debate
+export interface CompletionDeadlineErrors {
+  firstErroredField: 'day' | 'month' | 'year'
+  erroredFields: ('day' | 'month' | 'year')[]
+  message: string
+}
---
+++ server/utils/errorMessages.ts
@@ -0,0 +1,8 @@
+export default {
+  completionDeadline: {
+    dayEmpty: 'The date by which the service needs to be completed must include a day',
+    monthEmpty: 'The date by which the service needs to be completed must include a month',
+    yearEmpty: 'The date by which the service needs to be completed must include a year',
+    invalidDate: 'The date by which the service needs to be completed must be a real date',
+  },
+}
