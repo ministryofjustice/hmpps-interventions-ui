76d02d72867a3bd84baefbc1a35059ef4b5925ee
David Lantos <david.lantos@gmail.com>
2020-12-09 09:25:40 +0000

    [to revisit] Disable authorisation for test clients
    I'm not familiar with Spring Boot :)
    
    All Pact tests fail with
    `1.1) status: expected status of 200 but was 401` because all of the
    requests are unauthorised.
    
    I wanted to either
    
    1. create a state in the Pact test that all incoming requests are
       pre-authorised
    2. or remove the authentication filter from the request filter chain
    
    I couldn't do either of these tidily -- the other examples in the
    organisation all involve significant amount of extra code which I wanted
    to avoid.
    
    So for now, this is a workaround: the permission checks only run on
    non-test profiles.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/config/ResourceServerConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/config/ResourceServerConfiguration.kt
@@ -2,6 +2,7 @@ package uk.gov.justice.digital.hmpps.hmppsinterventionsservice.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
@@ -11,13 +12,24 @@ import org.springframework.security.oauth2.server.resource.authentication.JwtGra
 
 @Configuration
 @EnableWebSecurity
-class ResourceServerConfiguration : WebSecurityConfigurerAdapter() {
-  // todo: add custom token validator which calls token verification service
+@Profile("test")
+class BasicResourceServerConfiguration : WebSecurityConfigurerAdapter() {
   override fun configure(http: HttpSecurity) {
     http
       .sessionManagement()
       .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
       .and().csrf().disable()
+  }
+}
+
+@Configuration
+@EnableWebSecurity
+@Profile("!test")
+class ResourceServerConfiguration : BasicResourceServerConfiguration() {
+  // todo: add custom token validator which calls token verification service
+  override fun configure(http: HttpSecurity) {
+    super.configure(http)
+    http
       .authorizeRequests {
         it.antMatchers("/health/**", "/info", "/test/**").permitAll()
         it.anyRequest().hasAuthority("ROLE_INTERVENTIONS")
