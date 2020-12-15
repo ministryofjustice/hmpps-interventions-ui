f8e02601a141017c98e9f79bb95ae10068e5b253
Lawrence Forooghian <lawrence@dxw.com>
2020-12-15 14:08:23 +0000

    Add needs and requirements form
    Some of the error messages here contain information interpolated from
    the referral. I’m not sure whether it makes more sense for these to be
    interpolated by the form or the presenter - it maybe is presentation
    logic. But it was easier to do it in the form so that’s what I’ve done.
    Let’s see how that goes and whether we find ourselves needing to do
    anything more complex than just needing to stick a model value directly
    into a message.

---
+++ server/routes/referrals/needsAndRequirementsForm.ts
@@ -0,0 +1,78 @@
+import { Request } from 'express'
+import { Result, ValidationChain, ValidationError } from 'express-validator'
+import { DraftReferral } from '../../services/interventionsService'
+import errorMessages from '../../utils/errorMessages'
+import FormUtils from '../../utils/formUtils'
+
+export default class NeedsAndRequirementsForm {
+  private constructor(
+    private readonly request: Request,
+    private readonly result: Result<ValidationError>,
+    private readonly referral: DraftReferral
+  ) {}
+
+  static async createForm(request: Request, referral: DraftReferral): Promise<NeedsAndRequirementsForm> {
+    return new NeedsAndRequirementsForm(
+      request,
+      await FormUtils.runValidations({ request, validations: this.validations(referral) }),
+      referral
+    )
+  }
+
+  static validations(referral: DraftReferral): ValidationChain[] {
+    const firstName = referral.serviceUser?.firstName ?? ''
+
+    return [
+      FormUtils.yesNoRadioWithConditionalInputValidationChain({
+        radioName: 'needs-interpreter',
+        inputName: 'interpreter-language',
+        notSelectedErrorMessage: errorMessages.needsInterpreter.empty(firstName),
+        inputValidator: chain =>
+          chain.notEmpty({ ignore_whitespace: true }).withMessage(errorMessages.interpreterLanguage.empty(firstName)),
+      }),
+      FormUtils.yesNoRadioWithConditionalInputValidationChain({
+        radioName: 'has-additional-responsibilities',
+        inputName: 'when-unavailable',
+        notSelectedErrorMessage: errorMessages.hasAdditionalResponsibilities.empty(firstName),
+        inputValidator: chain =>
+          chain.notEmpty({ ignore_whitespace: true }).withMessage(errorMessages.whenUnavailable.empty(firstName)),
+      }),
+    ].reduce((a, b) => a.concat(b), [])
+  }
+
+  get isValid(): boolean {
+    return this.errors == null
+  }
+
+  get paramsForUpdate(): Partial<DraftReferral> {
+    return {
+      additionalNeedsInformation: this.request.body['additional-needs-information'],
+      accessibilityNeeds: this.request.body['accessibility-needs'],
+      needsInterpreter: this.needsInterpeter,
+      interpreterLanguage: this.needsInterpeter ? this.request.body['interpreter-language'] : null,
+      hasAdditionalResponsibilities: this.hasAdditionalResponsibilities,
+      whenUnavailable: this.hasAdditionalResponsibilities ? this.request.body['when-unavailable'] : null,
+    }
+  }
+
+  private get needsInterpeter(): boolean {
+    return this.request.body['needs-interpreter'] === 'yes'
+  }
+
+  private get hasAdditionalResponsibilities(): boolean {
+    return this.request.body['has-additional-responsibilities'] === 'yes'
+  }
+
+  get errors(): NeedsAndRequirementsError[] | null {
+    if (this.result.isEmpty()) {
+      return null
+    }
+
+    return this.result.array().map(validationError => ({ field: validationError.param, message: validationError.msg }))
+  }
+}
+
+export interface NeedsAndRequirementsError {
+  field: string
+  message: string
+}
--- server/utils/errorMessages.ts
+++ server/utils/errorMessages.ts
@@ -1,3 +1,7 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+// Explicit return types would make this file very messy and not much more
+// informational — everything returned is a string, since they’re messages!
+
 export default {
   completionDeadline: {
     dayEmpty: 'The date by which the service needs to be completed must include a day',
@@ -10,5 +14,17 @@ export default {
   },
   desiredOutcomes: {
     empty: 'Select desired outcomes',
+  },
+  needsInterpreter: {
+    empty: (name: string) => `Select yes if ${name} needs an interpreter`,
+  },
+  interpreterLanguage: {
+    empty: (name: string) => `Enter the language for which ${name} needs an interpreter`,
+  },
+  hasAdditionalResponsibilities: {
+    empty: (name: string) => `Select yes if ${name} has caring or employment responsibilities`,
+  },
+  whenUnavailable: {
+    empty: (name: string) => `Enter details of when ${name} will not be able to attend sessions`,
   },
 }
