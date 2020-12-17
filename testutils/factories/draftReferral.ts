b4720316beb859fa84a29e43be3863609a3b25f0
Lawrence Forooghian <lawrence@dxw.com>
2020-12-17 09:14:04 +0000

    Allow not passing an ID to serviceCategorySelected
    If we don’t care about which service category is selected, then the
    factory will just populate a representative ID.

--- testutils/factories/draftReferral.ts
+++ testutils/factories/draftReferral.ts
@@ -1,13 +1,15 @@
 import { Factory } from 'fishery'
 import { DraftReferral } from '../../server/services/interventionsService'
+import serviceCategoryFactory from './serviceCategory'
 
 class DraftReferralFactory extends Factory<DraftReferral> {
   justCreated() {
     return this
   }
 
-  serviceCategorySelected(serviceCategoryId: string) {
-    return this.params({ serviceCategoryId })
+  serviceCategorySelected(serviceCategoryId?: string) {
+    const resolvedServiceCategoryId = serviceCategoryId ?? serviceCategoryFactory.build().id
+    return this.params({ serviceCategoryId: resolvedServiceCategoryId })
   }
 
   completionDeadlineSet() {
