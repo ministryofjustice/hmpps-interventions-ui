56e90a8600132028108e8bd9fc89aa32b3289901
Tom Myers <tom.myers@digital.justice.gov.uk>
2020-12-17 10:53:24 +0000

    add index for created_by_userid column

--- src/main/resources/db/migration/V1_1__add_created_by_user_id.sql
+++ src/main/resources/db/migration/V1_1__add_created_by_user_id.sql
@@ -1,2 +1,4 @@
 alter table referral
-    add column created_by_userid text;
+    add column created_by_userid varchar(255);
+
+create index IX_referral_created_by_userid on referral (created_by_userid);
