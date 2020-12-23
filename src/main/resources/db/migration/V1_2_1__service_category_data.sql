8012bea53fda5a837048a07703a17933941d22b6
Tom Myers <tom.myers@digital.justice.gov.uk>
2020-12-23 16:23:00 +0000

    rename sql files for consistency

---
+++ src/main/resources/db/migration/V1_2_1__service_category_data.sql
@@ -0,0 +1,10 @@
+insert into service_category (id, name, created)
+    values ('428ee70f-3001-4399-95a6-ad25eaaede16', 'Accommodation', now()),
+           ('ca374ac3-84eb-4b91-bea7-9005398f426f', 'Education, Training and Employment (ETE)', now()),
+           ('96a63c39-4371-4f17-a6ec-265755f0cf7b', 'Finance, Benefits and Debt (FBD)', now()),
+           ('76bcdb97-1dea-41c1-a4f8-899d88e5d679', 'Dependency and Recovery (D&R)', now()),
+           ('b84f4eb7-4db0-477e-8c59-21027b3262c5', 'Lifestyle and Associates', now()),
+           ('9556a399-3529-4993-8030-41db2090555e', 'Family and Significant Others', now()),
+           ('8221a81c-08b2-4262-9c1a-0ab3c82cec8c', 'Emotional Wellbeing', now()),
+           ('c036826e-f077-49a5-8b33-601dca7ad479', 'Social Inclusion', now()),
+           ('c519b07a-f3b1-42aa-ad80-78828b72c14e', 'Women''s Specific', now());
