6baf6eab80ff76303baeaa7cf3380cd81677c9a0
Tom Myers <tom.myers@digital.justice.gov.uk>
2021-01-06 16:52:02 +0000

    revert to a custom error configuration that gives us more control over validation error processing

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/config/ErrorConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/config/ErrorConfiguration.kt
@@ -1,15 +1,74 @@
 package uk.gov.justice.digital.hmpps.hmppsinterventionsservice.config
 
+import com.fasterxml.jackson.annotation.JsonInclude
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.ResponseStatus
-import java.util.UUID
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.server.ResponseStatusException
 
-@ResponseStatus(value = HttpStatus.BAD_REQUEST)
-class ReferralBadIDException(private val id: String) : RuntimeException("could not parse id [id=${id}]")
+data class FieldError(
+  val field: String,
+  val error: String,
+)
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND)
-class ReferralNotFoundException(private val id: UUID) : RuntimeException("referral not found [id=${id}]")
+class ValidationError(override val message: String, val errors: List<FieldError>) : RuntimeException(message)
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND)
-class ServiceCategoryNotFoundException(private val id: UUID): RuntimeException("service category not found [id=${id}]")
+@RestControllerAdvice
+class ErrorConfiguration {
+  @ExceptionHandler(ValidationError::class)
+  fun handleValidationException(e: ValidationError): ResponseEntity<ErrorResponse> {
+    log.info("validation exception: {}", e.message)
+    return ResponseEntity
+      .status(HttpStatus.BAD_REQUEST)
+      .body(
+        ErrorResponse(
+          status = HttpStatus.BAD_REQUEST.value(),
+          error = "validation error",
+          message = e.message,
+          validationErrors = e.errors,
+        )
+      )
+  }
 
+  @ExceptionHandler(ResponseStatusException::class)
+  fun handleResponseException(e: ResponseStatusException): ResponseEntity<ErrorResponse> {
+    log.info("internal exception: {}", e.message)
+    return ResponseEntity
+      .status(e.status)
+      .body(
+        ErrorResponse(
+          status = e.status.value(),
+          error = e.status.reasonPhrase,
+          message = e.reason,
+        )
+      )
+  }
+
+  @ExceptionHandler(java.lang.Exception::class)
+  fun handleException(e: java.lang.Exception): ResponseEntity<ErrorResponse?>? {
+    log.error("unexpected exception: {}", e)
+    return ResponseEntity
+      .status(HttpStatus.INTERNAL_SERVER_ERROR)
+      .body(
+        ErrorResponse(
+          status = HttpStatus.INTERNAL_SERVER_ERROR.value(),
+          error = "unexpected error",
+          message = e.message,
+        )
+      )
+  }
+
+  companion object {
+    private val log = LoggerFactory.getLogger(ErrorConfiguration::class.java)
+  }
+}
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+data class ErrorResponse(
+  val status: Int,
+  val error: String,
+  val message: String?,
+  val validationErrors: List<FieldError>? = null,
+)
