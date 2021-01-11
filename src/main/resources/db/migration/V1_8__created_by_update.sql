b943bcf7caa886b172ab63a86624df788d22b6ee
Tom Myers <tom.myers@digital.justice.gov.uk>
2021-01-11 11:09:38 +0000

    fix latest migration to unblock deploy

--- src/main/resources/db/migration/V1_8__created_by_update.sql
+++ src/main/resources/db/migration/V1_8__created_by_update.sql
@@ -1,10 +1,14 @@
 alter table referral
-    add column created_by_user_auth_source varchar(255) not null;
+    add column created_by_user_auth_source varchar(255);
 
 update referral
-        set created_by_userid = 'unknown',
-        created_by_user_auth_source = 'unknown'
+    set created_by_userid = 'unknown'
     where created_by_userid = null;
 
+update referral
+    set created_by_user_auth_source = 'unknown'
+    where created_by_user_auth_source = null;
+
 alter table referral
-    alter column created_by_userid set not null;
+    alter column created_by_userid set not null,
+    alter column created_by_user_auth_source set not null;
