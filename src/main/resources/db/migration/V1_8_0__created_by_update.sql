8879095c8e5d8d9de60b72ca26258b02993dec44
Tom Myers <tom.myers@digital.justice.gov.uk>
2021-01-11 12:02:43 +0000

    split latest migration in two to fix deploy

--- src/main/resources/db/migration/V1_8_0__created_by_update.sql
+++ src/main/resources/db/migration/V1_8_0__created_by_update.sql
@@ -8,7 +8,3 @@ update referral
 update referral
     set created_by_user_auth_source = 'unknown'
     where created_by_user_auth_source = null;
-
-alter table referral
-    alter column created_by_userid set not null,
-    alter column created_by_user_auth_source set not null;
