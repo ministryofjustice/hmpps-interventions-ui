c3d8a117fe14fb7d5628ca09eebb47430f2ba3ce
Lawrence Forooghian <lawrence@dxw.com>
2021-01-18 16:44:11 +0000

    Add an integration spec for creating and submitting a referral
    This doesn’t use the real backend - everything is done with Wiremock. So
    it’s not so much an end-to-end test, more of an integration test to
    check that the UI is hooked up correctly.
    
    I decided to use a testing-library [1] style approach of avoiding
    targeting elements by IDs or other HTML attributes, instead preferring
    to use user-facing attributes like text. I saw this recommendation
    in some GitLab frontend testing advice [2], and, not having a strong opinion
    myself, thought it worth a go.
    
    I haven’t used any page objects here. I saw that we’ve used them here,
    and saw them recommended in [3]. But, I tried writing some and thought
    that it was kind of easier to tell what was going on as it is. Maybe
    will change opinion on this as we write more tests.
    
    [1] https://testing-library.com
    [2] https://docs.gitlab.com/ee/development/testing_guide/frontend_testing.html#how-to-query-dom-elements
    [3] https://sttp.site/chapters/testing-into-context/web-testing.html#page-objects

---
+++ integration_tests/integration/referral.spec.js
@@ -0,0 +1,140 @@
+import draftReferralFactory from '../../testutils/factories/draftReferral'
+import sentReferralFactory from '../../testutils/factories/sentReferral'
+import serviceCategoryFactory from '../../testutils/factories/serviceCategory'
+import serviceProviderFactory from '../../testutils/factories/serviceProvider'
+
+describe('Referral form', () => {
+  beforeEach(() => {
+    cy.task('reset')
+    cy.task('stubLogin')
+    cy.task('stubAuthUser')
+  })
+
+  it('User starts a referral, fills in the form, and submits it', () => {
+    cy.login()
+
+    const serviceCategory = serviceCategoryFactory.build({
+      name: 'accommodation',
+      desiredOutcomes: [
+        {
+          id: '1',
+          description: 'Service User makes progress in obtaining accommodation',
+        },
+        {
+          id: '2',
+          description: 'Service User is prevented from becoming homeless',
+        },
+      ],
+      complexityLevels: [
+        {
+          id: '1',
+          title: 'Low complexity',
+          description: 'Info about low complexity',
+        },
+        {
+          id: '2',
+          title: 'High complexity',
+          description: 'Info about high complexity',
+        },
+      ],
+    })
+
+    const serviceProvider = serviceProviderFactory.build({ name: 'Harmony Living' })
+
+    const draftReferral = draftReferralFactory.build({
+      serviceCategoryId: serviceCategory.id,
+      serviceProviderId: serviceProvider.id,
+      serviceUser: { firstName: 'Geoffrey' },
+    })
+
+    const sentReferral = sentReferralFactory.fromFields(draftReferral).build()
+
+    cy.stubCreateDraftReferral(draftReferral)
+    cy.stubGetServiceCategory(serviceCategory.id, serviceCategory)
+    cy.stubGetServiceProvider(serviceProvider.id, serviceProvider)
+    cy.stubGetDraftReferralsForUser([])
+    cy.stubGetDraftReferral(draftReferral.id, draftReferral)
+    cy.stubPatchDraftReferral(draftReferral.id, draftReferral)
+    cy.stubSendDraftReferral(draftReferral.id, sentReferral)
+    cy.stubGetSentReferral(sentReferral.id, sentReferral)
+
+    cy.visit('/referrals/start')
+
+    cy.contains('Start now').click()
+
+    cy.location('pathname').should('equal', `/referrals/${draftReferral.id}/form`)
+
+    cy.contains('Service user’s needs and requirements').click()
+
+    cy.location('pathname').should('equal', `/referrals/${draftReferral.id}/needs-and-requirements`)
+    cy.get('h1').contains('Geoffrey’s needs and requirements')
+
+    cy.contains('Additional information about Geoffrey’s needs').type(
+      'Geoffrey is currently sleeping on his aunt’s sofa'
+    )
+    cy.contains('Does Geoffrey have any other mobility, disability or accessibility needs?').type(
+      'He uses a wheelchair'
+    )
+    cy.withinFieldsetThatContains('Does Geoffrey need an interpreter?', () => {
+      cy.contains('Yes').click()
+    })
+    cy.contains('What language?').type('Spanish')
+    cy.withinFieldsetThatContains('Does Geoffrey have caring or employment responsibilities?', () => {
+      cy.contains('Yes').click()
+    })
+    cy.contains('Provide details of when Geoffrey will not be able to attend sessions').type(
+      'He works Mondays 9am - midday'
+    )
+
+    cy.contains('Save and continue').click()
+
+    cy.location('pathname').should('equal', `/referrals/${draftReferral.id}/form`)
+    cy.contains('Select desired outcomes').click()
+
+    cy.location('pathname').should('equal', `/referrals/${draftReferral.id}/desired-outcomes`)
+    cy.get('h1').contains('What are the desired outcomes for the accommodation service?')
+
+    cy.contains('Service User makes progress in obtaining accommodation').click()
+    cy.contains('Service User is prevented from becoming homeless').click()
+
+    cy.contains('Save and continue').click()
+
+    cy.location('pathname').should('equal', `/referrals/${draftReferral.id}/complexity-level`)
+    cy.get('h1').contains('What is the complexity level for the accommodation service?')
+    cy.contains('Low complexity').click()
+
+    cy.contains('Save and continue').click()
+
+    cy.location('pathname').should('equal', `/referrals/${draftReferral.id}/completion-deadline`)
+    cy.get('h1').contains('What date does the accommodation service need to be completed by?')
+    cy.contains('Day').type('15')
+    cy.contains('Month').type('8')
+    cy.contains('Year').type('2021')
+
+    cy.contains('Save and continue').click()
+
+    cy.location('pathname').should('equal', `/referrals/${draftReferral.id}/rar-days`)
+    cy.get('h1').contains('Are you using RAR days for the accommodation service?')
+    cy.contains('Yes').click()
+    cy.contains('What is the maximum number of RAR days for the accommodation service?').type('10')
+
+    cy.contains('Save and continue').click()
+
+    cy.location('pathname').should('equal', `/referrals/${draftReferral.id}/further-information`)
+    cy.get('h1').contains('Do you have further information for the accommodation service provider? (optional)')
+    cy.get('textarea').type('Some information about Geoffrey')
+
+    cy.contains('Save and continue').click()
+
+    cy.location('pathname').should('equal', `/referrals/${draftReferral.id}/form`)
+
+    cy.get('a').contains('Check your answers').click()
+    cy.location('pathname').should('equal', `/referrals/${draftReferral.id}/check-answers`)
+
+    cy.contains('Submit referral').click()
+    cy.location('pathname').should('equal', `/referrals/${sentReferral.id}/confirmation`)
+
+    cy.contains('We’ve sent your referral to Harmony Living')
+    cy.contains(sentReferral.referenceNumber)
+  })
+})
