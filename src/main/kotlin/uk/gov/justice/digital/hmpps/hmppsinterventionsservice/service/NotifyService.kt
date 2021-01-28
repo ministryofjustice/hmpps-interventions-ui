4bbb5b670878a67d0050c5d924accaa467c59e03
Tom Myers <tom.myers@digital.justice.gov.uk>
2021-01-28 19:53:11 +0000

    implement the event listener interface in the notify service instead of a seperate class

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/service/NotifyService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/service/NotifyService.kt
@@ -2,32 +2,53 @@ package uk.gov.justice.digital.hmpps.hmppsinterventionsservice.service
 
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.ApplicationListener
 import org.springframework.stereotype.Service
+import org.springframework.web.util.UriComponentsBuilder
+import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.events.ReferralEvent
+import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.events.ReferralEventType
 import uk.gov.service.notify.NotificationClient
-import java.net.URI
 
 @Service
 class NotifyService(
   @Value("\${notify.enabled}") private val enabled: Boolean,
   @Value("\${notify.templates.referral-sent}") private val referralSentTemplateID: String,
+  @Value("\${interventions-ui.baseurl}") private val interventionsUIBaseURL: String,
+  @Value("\${interventions-ui.locations.sent-referral}") private val interventionsUISentReferralLocation: String,
   private val client: NotificationClient,
-) {
-  fun referralSent(referenceNumber: String, location: URI, recipientEmail: String) {
-    if (enabled) {
-      try {
-        client.sendEmail(
+) : ApplicationListener<ReferralEvent> {
+  override fun onApplicationEvent(event: ReferralEvent) {
+    when (event.type) {
+      ReferralEventType.SENT -> {
+        val location = UriComponentsBuilder.fromHttpUrl(interventionsUIBaseURL)
+          .path(interventionsUISentReferralLocation)
+          .buildAndExpand(event.referral.id)
+          .toUri()
+
+        sendEmail(
           referralSentTemplateID,
-          recipientEmail,
-          mapOf(
-            "referenceNumber" to referenceNumber,
-            "referralUrl" to location.toString(),
-          ),
-          null,
+          "tom.myers@digital.justice.gov.uk", // fixme: this email address will eventually come from the provider associated with the referral
+          mapOf("referenceNumber" to event.referral.referenceNumber!!, "referralUrl" to location.toString())
         )
-      } catch (e: Exception) {
-        // fixme: this failure is super important and we need a better way to reason about async errors of this nature
-        log.error("referral sent notification failed", e)
       }
+    }
+  }
+
+  private fun sendEmail(templateID: String, emailAddress: String, personalisation: Map<String, String>) {
+    if (!enabled) {
+      return
+    }
+
+    try {
+      client.sendEmail(
+        templateID,
+        emailAddress,
+        personalisation,
+        null,
+      )
+    } catch (e: Exception) {
+      // fixme: this failure is super important and we need a better way to reason about async errors of this nature
+      log.error("email notification failed", e)
     }
   }
 
