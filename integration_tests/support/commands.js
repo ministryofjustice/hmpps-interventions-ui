1c49658af1f38b11ae96dce293ccf0e0e9d54e76
George Eaton <george@dxw.com>
2021-02-03 16:31:34 +0000

    Write own getTable cypress command
    Using the `getTable` package has caused our build to fail as it's trying
    to install multiple versions of Cypress, so we're adding the
    implementation to our codebase instead.
    
    This implementation has been shamelessly copied from
    https://github.com/roggerfe/cypress-get-table/blob/master/src/index.js.

--- integration_tests/support/commands.js
+++ integration_tests/support/commands.js
@@ -6,3 +6,27 @@ Cypress.Commands.add('login', () => {
 Cypress.Commands.add('withinFieldsetThatContains', (text, action) => {
   cy.contains(text).parent('fieldset').within(action)
 })
+
+const getTable = (subject, options = {}) => {
+  if (subject.get().length > 1) {
+    throw new Error(`Selector "${subject.selector}" returned more than 1 element.`)
+  }
+
+  const tableElement = subject.get()[0]
+  const headers = [...tableElement.querySelectorAll('thead th')].map(e => e.textContent)
+
+  const rows = [...tableElement.querySelectorAll('tbody tr')].map(row => {
+    return [...row.querySelectorAll('td')].map(e => e.textContent)
+  })
+
+  return rows.map(row =>
+    row.reduce((acc, curr, index) => {
+      if (options.onlyColumns && !options.onlyColumns.includes(headers[index])) {
+        return { ...acc }
+      }
+      return { ...acc, [headers[index]]: curr }
+    }, {})
+  )
+}
+
+Cypress.Commands.add('getTable', { prevSubject: true }, getTable)
