1495630519a6e537a134ed2ab8456e101b672856
Lawrence Forooghian <lawrence@dxw.com>
2021-02-03 09:30:49 +0000

    Add ability to place links in dashboard table
    We'll use this for linking from the reference number to a referral
    details page.

--- server/routes/serviceProviderReferrals/dashboardPresenter.test.ts
+++ server/routes/serviceProviderReferrals/dashboardPresenter.test.ts
@@ -41,16 +41,16 @@ describe(DashboardPresenter, () => {
 
       expect(presenter.tableRows).toEqual([
         [
-          { text: '26 Jan 2021', sortValue: '2021-01-26' },
-          { text: 'ABCABCA1', sortValue: null },
-          { text: 'George Michael', sortValue: 'michael, george' },
-          { text: 'Accommodation', sortValue: null },
+          { text: '26 Jan 2021', sortValue: '2021-01-26', href: null },
+          { text: 'ABCABCA1', sortValue: null, href: null },
+          { text: 'George Michael', sortValue: 'michael, george', href: null },
+          { text: 'Accommodation', sortValue: null, href: null },
         ],
         [
-          { text: '13 Sep 2020', sortValue: '2020-09-13' },
-          { text: 'ABCABCA2', sortValue: null },
-          { text: 'Jenny Jones', sortValue: 'jones, jenny' },
-          { text: 'Social inclusion', sortValue: null },
+          { text: '13 Sep 2020', sortValue: '2020-09-13', href: null },
+          { text: 'ABCABCA2', sortValue: null, href: null },
+          { text: 'Jenny Jones', sortValue: 'jones, jenny', href: null },
+          { text: 'Social inclusion', sortValue: null, href: null },
         ],
       ])
     })
