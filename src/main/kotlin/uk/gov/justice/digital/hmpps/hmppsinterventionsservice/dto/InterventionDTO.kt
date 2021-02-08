df61b6f3084b953cb5f27e9485a964ff1e8f3f2e
karen <karen.miller@digital.justice.gov.uk>
2021-02-08 13:34:11 +0000

    Add ability to get all interventions

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/dto/InterventionDTO.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/dto/InterventionDTO.kt
@@ -1,17 +1,49 @@
 package uk.gov.justice.digital.hmpps.hmppsinterventionsservice.dto
 
+import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.jpa.entity.ContractEligibility
 import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.jpa.entity.Intervention
+import java.util.UUID
 
 data class InterventionDTO(
+  val id: UUID,
   val title: String,
   val description: String,
+  val pccRegions: List<PCCRegionDTO>,
+  val serviceCategory: ServiceCategoryDTO,
+  val serviceProvider: ServiceProviderDTO,
+  val eligibility: ContractEligibilityDTO,
 ) {
   companion object {
 
-    fun from(intervention: Intervention): InterventionDTO {
+    fun from(intervention: Intervention, pccRegions: List<PCCRegionDTO>): InterventionDTO {
+      val contract = intervention.dynamicFrameworkContract
+      val contractEligibility = contract.contractEligibility
       return InterventionDTO(
+        id = intervention.id!!,
         title = intervention.title,
         description = intervention.description,
+        pccRegions = pccRegions,
+        serviceCategory = ServiceCategoryDTO.from(contract.serviceCategory),
+        serviceProvider = ServiceProviderDTO.from(contract.serviceProvider),
+        eligibility = ContractEligibilityDTO.from(contractEligibility),
+      )
+    }
+  }
+}
+
+class ContractEligibilityDTO(
+  val minimumAge: Int,
+  val maximumAge: Int?,
+  val allowsFemale: Boolean,
+  val allowsMale: Boolean,
+) {
+  companion object {
+    fun from(contractEligibility: ContractEligibility): ContractEligibilityDTO {
+      return ContractEligibilityDTO(
+        minimumAge = contractEligibility.minimumAge,
+        maximumAge = contractEligibility.maximumAge,
+        allowsFemale = contractEligibility.allowsFemale,
+        allowsMale = contractEligibility.allowsMale
       )
     }
   }
--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/service/InterventionService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/service/InterventionService.kt
@@ -2,18 +2,38 @@ package uk.gov.justice.digital.hmpps.hmppsinterventionsservice.service
 
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
+import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.dto.InterventionDTO
+import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.dto.PCCRegionDTO
 import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.jpa.entity.AuthGroupID
 import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.jpa.entity.Intervention
 import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.jpa.repository.InterventionRepository
+import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.jpa.repository.PCCRegionRepository
 import java.util.UUID
 
 @Service
-class InterventionService(val repository: InterventionRepository) {
+class InterventionService(
+  val pccRegionRepository: PCCRegionRepository,
+  val repository: InterventionRepository
+) {
 
-  fun getIntervention(id: UUID): Intervention? {
-    return repository.findByIdOrNull(id)
+  fun getIntervention(id: UUID): InterventionDTO? {
+    return repository.findByIdOrNull(id)?.let { InterventionDTO.from(it, getPCCRegions(it)) }
   }
   fun getInterventionsForServiceProvider(id: AuthGroupID): List<Intervention> {
     return repository.findByDynamicFrameworkContractServiceProviderId(id)
+  }
+  fun getAllInterventions(): List<InterventionDTO> {
+    return repository.findAll().map {
+      InterventionDTO.from(it, getPCCRegions(it))
+    }
+  }
+  private fun getPCCRegions(intervention: Intervention): List<PCCRegionDTO> {
+    val contract = intervention.dynamicFrameworkContract
+    return if (contract.pccRegion != null) {
+      listOf(PCCRegionDTO.from(contract.pccRegion))
+    } else {
+      val pccRegions = pccRegionRepository.findAllByNpsRegionId(contract.npsRegion!!.id)
+      pccRegions.map { PCCRegionDTO.from(it) }
+    }
   }
 }
