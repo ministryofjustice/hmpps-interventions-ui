10e1b4520a10f1642414f48cd76de9bc283099ff
David Lantos <david.lantos@gmail.com>
2021-02-18 18:55:45 +0000

    Generate reference numbers without ambiguous chars

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/service/ReferralReferenceGenerator.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/service/ReferralReferenceGenerator.kt
@@ -3,11 +3,24 @@ package uk.gov.justice.digital.hmpps.hmppsinterventionsservice.service
 import org.springframework.stereotype.Component
 
 @Component
-class ReferralReferenceGenerator {
-  private val prefixChars = ('A'..'Z').toList()
-  private val numbers = ('0'..'9').toList()
+class ReferralReferenceGenerator(
+  private val prefixChars: List<Char> = ('A'..'Z').toList(),
+  private val numbers: List<Char> = ('0'..'9').toList(),
+) {
+  private val ambiguousPairs = listOf(
+    Pair("0", "O"),
+    Pair("1", "I"),
+    Pair("5", "S"),
+    Pair("8", "B"),
+  )
 
   fun generate(categoryName: String): String {
+    return generateSequence { newReference(categoryName) }
+      .filterNot { ambiguous(it) }
+      .first()
+  }
+
+  private fun newReference(categoryName: String): String {
     return buildString {
       append(prefix())
       append(numbers())
@@ -17,4 +30,8 @@ class ReferralReferenceGenerator {
 
   private fun prefix(): String = buildString { while (length < 2) append(prefixChars.random()) }
   private fun numbers(): String = buildString { while (length < 4) append(numbers.random()) }
+
+  private fun ambiguous(candidate: String): Boolean {
+    return ambiguousPairs.any { pair -> candidate.contains(pair.first) && candidate.contains(pair.second) }
+  }
 }
--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/service/ReferralReferenceGeneratorTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/service/ReferralReferenceGeneratorTest.kt
@@ -14,6 +14,30 @@ class ReferralReferenceGeneratorTest {
   }
 
   @Test
+  fun `references must avoid similar looking letters and numbers`() {
+    data class Example(val prefixChars: String, val numbers: String, val doesNotContain: String)
+
+    val examples = listOf(
+      Example("O", "07", "0"),
+      Example("I", "17", "1"),
+      Example("S", "57", "5"),
+      Example("B", "87", "8"),
+      Example("OX", "0", "O"),
+      Example("IX", "1", "I"),
+      Example("SX", "5", "S"),
+      Example("BX", "8", "B"),
+    )
+
+    examples.forEach { ex ->
+      val g = ReferralReferenceGenerator(prefixChars = ex.prefixChars.toList(), numbers = ex.numbers.toList())
+      assertThat(g.generate("name")).doesNotContain(
+        ex.doesNotContain,
+        "Characters in ${ex.prefixChars} and ${ex.numbers} are similar, avoid '${ex.doesNotContain}' to remain unambiguous"
+      )
+    }
+  }
+
+  @Test
   fun `reference postfix is the capitalised first two (unicode) letters of the given category`() {
     assertThat(generator.generate("Category")).endsWith("CA")
     assertThat(generator.generate("accommodation")).endsWith("AC")
