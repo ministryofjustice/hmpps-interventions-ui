c16e067ac211515145f66da1aac2e0e894627895
rajivmahal <rajiv.mahal@digital.justice.gov.uk>
2021-02-19 17:07:47 +0000

    IC-1217 - Added test utils ActionPlanFactory

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/util/ActionPlanFactory.kt
@@ -0,0 +1,42 @@
+package uk.gov.justice.digital.hmpps.hmppsinterventionsservice.util
+
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager
+import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.jpa.entity.ActionPlan
+import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.jpa.entity.ActionPlanActivity
+import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.jpa.entity.AuthUser
+import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.jpa.entity.DesiredOutcome
+import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.jpa.entity.Referral
+import java.time.OffsetDateTime
+import java.util.UUID
+
+class ActionPlanFactory(em: TestEntityManager) : EntityFactory(em) {
+  private val authUserFactory = AuthUserFactory(em)
+  private val referralFactory = ReferralFactory(em)
+
+  fun create(
+    id: UUID? = null,
+    referral: Referral? = null,
+    numberOfSessions: Int? = null,
+    createdAt: OffsetDateTime? = null,
+    createdBy: AuthUser? = null,
+    singleActivityDescription: String,
+    singleActivityDesiredOutcome: DesiredOutcome
+  ): ActionPlan {
+    return save(
+      ActionPlan(
+        id = id ?: UUID.randomUUID(),
+        referral = referral ?: referralFactory.create(),
+        numberOfSessions = numberOfSessions,
+        createdAt = createdAt ?: OffsetDateTime.now(),
+        createdBy = createdBy ?: authUserFactory.create(),
+        activities = listOf(
+          ActionPlanActivity(
+            description = singleActivityDescription,
+            createdAt = OffsetDateTime.now(),
+            desiredOutcome = singleActivityDesiredOutcome
+          )
+        )
+      )
+    )
+  }
+}
