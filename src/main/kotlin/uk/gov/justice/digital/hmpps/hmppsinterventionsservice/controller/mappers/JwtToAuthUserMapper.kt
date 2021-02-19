6d7318e1999ff165405dcbd8f43fb9c13a005de7
rajivmahal <rajiv.mahal@digital.justice.gov.uk>
2021-02-19 15:52:27 +0000

    IC-1217 - Reverted change that should not have been committed

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/controller/mappers/JwtToAuthUserMapper.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/controller/mappers/JwtToAuthUserMapper.kt
@@ -2,6 +2,7 @@ package uk.gov.justice.digital.hmpps.hmppsinterventionsservice.controller.mapper
 
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken
 import org.springframework.stereotype.Component
+import org.springframework.web.server.ServerWebInputException
 import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.jpa.entity.AuthUser
 
 @Component
@@ -10,7 +11,6 @@ class JwtToAuthUserMapper {
   fun parseAuthUserToken(authentication: JwtAuthenticationToken): AuthUser {
     // note: this does not allow tokens for client_credentials grant types use this API
 
-    /*
     val userID = authentication.token.getClaimAsString("user_id")
       ?: throw ServerWebInputException("no 'user_id' claim in authentication token")
 
@@ -21,7 +21,5 @@ class JwtToAuthUserMapper {
       ?: throw ServerWebInputException("no 'auth_source' claim in authentication token")
 
     return AuthUser(id = userID, authSource = authSource, userName = userName)
-    */
-    return AuthUser("2500128586", "delius", "bernard.beaks")
   }
 }
