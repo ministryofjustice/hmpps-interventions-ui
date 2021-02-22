486795d57eeb42625635a6c2f2c3189d0dd75f6b
rajivmahal <rajiv.mahal@digital.justice.gov.uk>
2021-02-22 12:44:01 +0000

    IC-1217 - Correction to data init script

--- src/main/resources/db/local/V99_3__created_action_plans.sql
+++ src/main/resources/db/local/V99_3__created_action_plans.sql
@@ -1,5 +1,5 @@
-insert into action_plan (id, referral_id, number_of_sessions, created_by_id, created_at, submitted_by_id, submitted_at, approved_by_id, approved_at)
-values ('700754aa-d868-4347-9c0f-50690773014e', '81d754aa-d868-4347-9c0f-50690773014e', 3, '8751622134', '2020-12-07 18:02:01.599803+00', null, null, null, null);
+insert into action_plan (id, referral_id, number_of_sessions, created_by_id, created_at, submitted_by_id, submitted_at)
+values ('700754aa-d868-4347-9c0f-50690773014e', '81d754aa-d868-4347-9c0f-50690773014e', 3, '8751622134', '2020-12-07 18:02:01.599803+00', null, null);
 
 insert into action_plan_activity (id, desired_outcome_id, description, created_at)
 values ('700754aa-d868-4347-9c0f-50690773014e', '301ead30-30a4-4c7c-8296-2768abfb59b5', 'Identify any outstanding anger management barriers', '2020-12-07 18:02:02.599803+00'),
