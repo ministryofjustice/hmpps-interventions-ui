554b5fcfae36e3e8a0cf0d9f17ddc2cf36ff271f
Tom Myers <tom.myers@digital.justice.gov.uk>
2021-02-23 17:04:30 +0000

    add service method to get user details from HMPPS Auth API

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/service/HMPPSAuthService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/service/HMPPSAuthService.kt
@@ -2,22 +2,48 @@ package uk.gov.justice.digital.hmpps.hmppsinterventionsservice.service
 
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.util.UriComponentsBuilder
+import reactor.core.publisher.Mono
 import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.jpa.entity.AuthGroupID
 import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.jpa.entity.AuthUser
 
-val AuthServiceProviderGroupPrefix = "INT_SP_"
+class UnverifiedEmailException : RuntimeException()
 
-data class AuthGroup(
+data class UserDetail(
+  val firstName: String,
+  val email: String,
+)
+
+private const val AuthServiceProviderGroupPrefix = "INT_SP_"
+
+private data class AuthGroupResponse(
   val groupCode: String,
   val groupName: String,
 )
 
+private data class AuthUserDetailResponse(
+  val firstName: String,
+  val email: String,
+  val verified: Boolean,
+)
+
+private data class UserEmailResponse(
+  val email: String,
+)
+
+private data class UserDetailResponse(
+  val name: String,
+)
+
 @Service
 class HMPPSAuthService(
-  @Value("\${hmppsauth.api.locations.user-groups}") private val userGroupsLocation: String,
+  @Value("\${hmppsauth.api.locations.auth-user-groups}") private val authUserGroupsLocation: String,
+  @Value("\${hmppsauth.api.locations.auth-user-detail}") private val authUserDetailLocation: String,
+  @Value("\${hmppsauth.api.locations.user-email}") private val userEmailLocation: String,
+  @Value("\${hmppsauth.api.locations.user-detail}") private val userDetailLocation: String,
   private val hmppsAuthApiWebClient: WebClient,
 ) {
   fun getServiceProviderOrganizationForUser(user: AuthUser): AuthGroupID? {
@@ -25,13 +51,13 @@ class HMPPSAuthService(
       return null
     }
 
-    val url = UriComponentsBuilder.fromPath(userGroupsLocation)
+    val url = UriComponentsBuilder.fromPath(authUserGroupsLocation)
       .buildAndExpand(user.userName)
       .toString()
 
     val groups = hmppsAuthApiWebClient.get().uri(url)
       .retrieve()
-      .bodyToFlux(AuthGroup::class.java)
+      .bodyToFlux(AuthGroupResponse::class.java)
       .collectList().block()
 
     val serviceProviderOrgs = groups
@@ -42,6 +68,38 @@ class HMPPSAuthService(
     // which represent SP admins managing subcontractor organizations.
     // for now we can assume there is only one group per user.
     return if (serviceProviderOrgs.isEmpty()) null else serviceProviderOrgs[0]
+  }
+
+  fun getUserDetail(user: AuthUser): UserDetail {
+    return if (user.authSource == "auth") {
+      val url = UriComponentsBuilder.fromPath(authUserDetailLocation).buildAndExpand(user.userName).toString()
+      hmppsAuthApiWebClient.get().uri(url)
+        .retrieve()
+        .bodyToMono(AuthUserDetailResponse::class.java)
+        .map {
+          if (!it.verified) {
+            throw UnverifiedEmailException()
+          }
+          UserDetail(it.firstName, it.email)
+        }
+        .block()
+    } else {
+      val detailUrl = UriComponentsBuilder.fromPath(userDetailLocation).buildAndExpand(user.userName).toString()
+      val emailUrl = UriComponentsBuilder.fromPath(userEmailLocation).buildAndExpand(user.userName).toString()
+      Mono.zip(
+        hmppsAuthApiWebClient.get().uri(detailUrl)
+          .retrieve()
+          .bodyToMono(UserDetailResponse::class.java)
+          .map { it.name.substringBefore(' ') },
+        hmppsAuthApiWebClient.get().uri(emailUrl)
+          .retrieve()
+          .onStatus({ it.equals(HttpStatus.NO_CONTENT) }, { Mono.error(UnverifiedEmailException()) })
+          .bodyToMono(UserEmailResponse::class.java)
+          .map { it.email }
+      )
+        .map { UserDetail(it.t1, it.t2) }
+        .block()
+    }
   }
 
   companion object {
