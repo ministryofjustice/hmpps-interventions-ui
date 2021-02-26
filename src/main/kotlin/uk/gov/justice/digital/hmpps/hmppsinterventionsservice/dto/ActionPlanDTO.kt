c19640f541bdb802e06ff524c6271da2a24e6fdc
rajivmahal <rajiv.mahal@digital.justice.gov.uk>
2021-02-26 12:11:36 +0000

    IC-535 Clean up of dtos

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/dto/ActionPlanDTO.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/dto/ActionPlanDTO.kt
@@ -53,25 +53,3 @@ data class ActionPlanActivityDTO(
     }
   }
 }
-
-data class DraftActionPlanDTO(
-  override val id: UUID,
-  override val referralId: UUID,
-  override val numberOfSessions: Int?,
-  override val activities: List<ActionPlanActivityDTO> = emptyList(),
-  val createdBy: AuthUserDTO,
-  val createdAt: OffsetDateTime
-) : BaseActionPlanDTO(id, referralId, numberOfSessions, activities) {
-  companion object {
-    fun from(actionPlan: ActionPlan): DraftActionPlanDTO {
-      return DraftActionPlanDTO(
-        id = actionPlan.id,
-        referralId = actionPlan.referral.id,
-        numberOfSessions = actionPlan.numberOfSessions,
-        activities = actionPlan.activities.map { ActionPlanActivityDTO.from(it) },
-        createdBy = AuthUserDTO.from(actionPlan.createdBy),
-        createdAt = actionPlan.createdAt,
-      )
-    }
-  }
-}
