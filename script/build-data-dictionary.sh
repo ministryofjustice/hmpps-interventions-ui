bb77daa095e69b79be6a81ea5b5f71461fa8be99
David Lantos <david.lantos@gmail.com>
2021-03-04 15:04:17 +0000

    Build the data dictionary as a CI job
    We are gravitating towards "self-describing" services, which we can
    interrogate for metadata
    
    We can use our database schema for the "data dictionary" as the comments
    on the tables and columns, together with their types and names give us a
    reasonable starting ground
    
    The result is packaged up as a static site, using `nginx` and published
    as a separate docker image

---
+++ script/build-data-dictionary.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -e
+basedir=$(dirname "$0")
+jardir="$basedir/libs"
+schemadir="$basedir/schema"
+
+mkdir -p "$jardir" "$schemadir"
+
+wget https://github.com/schemaspy/schemaspy/releases/download/v6.1.0/schemaspy-6.1.0.jar -O "$jardir/schemaspy.jar"
+wget https://jdbc.postgresql.org/download/postgresql-42.2.19.jar -O "$jardir/postgresql.jar"
+
+java -jar "$jardir/schemaspy.jar" -t pgsql -dp "$jardir/postgresql.jar" \
+  -db "${POSTGRES_DB:-interventions}" -host "localhost" -port "5432" -u "postgres" -p "password" \
+  -o "$schemadir"
