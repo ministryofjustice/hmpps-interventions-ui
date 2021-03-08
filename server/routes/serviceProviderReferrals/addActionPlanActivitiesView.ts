9dbc3a2b595af493cfa92b5c41f9a7a1c0a91aed
George Eaton <george@dxw.com>
2021-03-08 16:40:01 +0000

    Add textareas to add activities view
    This will render a govuk textarea for each desired outcome selected on
    the referral, with a button to submit an activity for that desired
    outcome. Next we’ll build the controller action to handle this
    submission.
    
    Co-authored-by: Lawrence Forooghian <lawrence@dxw.com>

--- server/routes/serviceProviderReferrals/addActionPlanActivitiesView.ts
+++ server/routes/serviceProviderReferrals/addActionPlanActivitiesView.ts
@@ -1,3 +1,4 @@
+import ViewUtils from '../../utils/viewUtils'
 import AddActionPlanActivitiesPresenter from './addActionPlanActivitiesPresenter'
 
 export default class AddActionPlanActivitiesView {
@@ -8,7 +9,25 @@ export default class AddActionPlanActivitiesView {
       'serviceProviderReferrals/actionPlan/addActivities',
       {
         presenter: this.presenter,
+        addActivityTextareaArgs: this.addActivityTextareaArgs,
       },
     ]
+  }
+
+  private addActivityTextareaArgs = (index: number) => {
+    const desiredOutcome = this.presenter.desiredOutcomes[index]!
+
+    return {
+      name: 'description',
+      id: `description-${index + 1}`,
+      label: {
+        html: `<h4 class="govuk-heading-s">${ViewUtils.escape(
+          `Activity ${desiredOutcome.activities.length + 1}`
+        )}</h4>`,
+      },
+      hint: {
+        text: 'What activity will you deliver to achieve this outcome?',
+      },
+    }
   }
 }
