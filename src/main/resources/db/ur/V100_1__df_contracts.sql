a6fb7acf38dcda9fd4dedaa193def6725dddcf4f
Tom Myers <tom.myers@digital.justice.gov.uk>
2021-03-21 22:56:13 +0000

    add sample data for user research testing environment

---
+++ src/main/resources/db/ur/V100_1__df_contracts.sql
@@ -0,0 +1,10 @@
+insert into dynamic_framework_contract (id, service_category_id, service_provider_id, start_date, end_date, nps_region_id, pcc_region_id, allows_female, allows_male, minimum_age, maximum_age)
+    values
+           -- emotional wellbeing service in cleveland
+           ('952eb687-a4a7-43b1-9d93-1b1a0c8cee5e', '8221a81c-08b2-4262-9c1a-0ab3c82cec8c', 'BETTER_LTD', '2021-06-24', '2022-06-24', null, 'cleveland', false, true, 18, null),
+           -- ETE service for all of yorkshire
+           ('21ef4732-73e6-486b-832c-9f49165d40ab', 'ca374ac3-84eb-4b91-bea7-9005398f426f', 'SAFE_LIVING', '2021-06-24', '2022-06-24', 'C', null, false, true, 18, null),
+           -- accommodation service for the midlands
+           ('7f5a2fb5-e3af-4395-9f38-c7c23dd8bec0', '428ee70f-3001-4399-95a6-ad25eaaede16', 'HARMONY_LIVING', '2021-06-24', '2022-06-24', 'F', null, false, true, 18, null),
+           -- ETE service for the midlands
+           ('9653676a-a51f-48cf-8541-444af11fe18b', 'ca374ac3-84eb-4b91-bea7-9005398f426f', 'NEW_BEGINNINGS', '2021-06-24', '2022-06-24', 'F', null, false, true, 18, null);
---
+++ src/main/resources/db/ur/V100_3_1__delius_users.sql
@@ -0,0 +1,4 @@
+insert into auth_user
+            (id, user_name, auth_source)
+        values
+        ('2500104586','TomMyersDevl','delius');
