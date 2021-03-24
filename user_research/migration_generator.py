81ae8f3be7ae0bdac986436c6a8192511fa486ea
Tom Myers <tom.myers@digital.justice.gov.uk>
2021-03-24 21:18:02 +0000

    Update user research seed with longer descriptions

--- user_research/migration_generator.py
+++ user_research/migration_generator.py
@@ -35,10 +35,62 @@ contracts = [
 ]
 
 interventions = [
-    ('0e4dcc37-e3f5-4c44-b043-887dda3a2baa', '952eb687-a4a7-43b1-9d93-1b1a0c8cee5e', '2021-03-19', 'Personal Wellbeing', 'Supporting services users around all areas of PW to enhance motivation and allow opportunity for SU change. The project consists of various external 1:1/group work activity across the region which directly link to SU outcome expectation.'),
-    ('ecdc6c7e-f04b-49b1-871d-3f9618555c3d', '21ef4732-73e6-486b-832c-9f49165d40ab', '2021-03-19', 'Employment, Training & Education', 'Support to secure employment, qualifications, training opportunities for people on release from custody or serving a community order. Pre and post-release support available.'),
-    ('5f0a4d93-c26e-439d-b261-0d8d6338e77f', '7f5a2fb5-e3af-4395-9f38-c7c23dd8bec0', '2021-03-19', 'Accommodation', 'Home Trust Ltd. offers support with sourcing and/or sustaining appropriate accommodation. We offer a range of interventions to help with all accommodation-related support needs.'),
-    ('11f06a1d-da75-4ca3-bb39-b7e848fb7612', '9653676a-a51f-48cf-8541-444af11fe18b', '2021-03-19', 'Employment, Training & Education', 'New Beginnings will provide ETE services to empower and motivate SUs to develop towards and take up education, training and employment.'),
+    (
+        '0e4dcc37-e3f5-4c44-b043-887dda3a2baa',
+        '952eb687-a4a7-43b1-9d93-1b1a0c8cee5e',
+        '2021-03-19',
+        'Emotional Wellbeing',
+        'Supporting services users around all areas of PW to enhance motivation and allow opportunity for SU change. The project consists of various external 1:1/group work activity across the region which directly link to SU outcome expectation.',
+    ),
+    (
+        'ecdc6c7e-f04b-49b1-871d-3f9618555c3d',
+        '21ef4732-73e6-486b-832c-9f49165d40ab',
+        '2021-03-19',
+        'Employment, Training & Education',
+        '''Support to secure employment, qualifications, training opportunities for people on release from custody or serving a community order. Pre and post-release support available.
+
+Our service assesses the ETE needs and interests of individuals referred to us and jointly with them we develop an action plan for success. We offer a range of interventions from light touch information, advice and guidance for those seeking a career change or wanting to develop their skills, through to intensive bespoke activities for those furthest from the job market.
+
+Initially we will work on building a persons knowledge , confidence , motivation and desire to access ETE activities and interventions and will maintain this focus throughout their time with us on the Programme. Tailored 1-1 and groupwork interventions as well as access to a bespoke online learning portal that participants can access (all supported by mentors if required) are all designed to enable them to develop their skills and knowledge.
+
+We source and manage a range of voluntary placements with a diverse range of large employers, social enterprise, community groups and training providers that participants can access when ready, which enables them to gain real work experience, tangible skills and the confidence and motivation to make the leap into full time employment and education.
+
+Our Community Partnership Network engages with employers and colleges across the region to ensure we have access to the latest jobs, courses and opportunities that we can signpost people to and support them to get there.
+
+We focus on the individual, provide wrap around support at the appropriate level and our aim is to motivate, mentor and to enable people to make a 'step change' in their lives.''',
+    ),
+    (
+        '5f0a4d93-c26e-439d-b261-0d8d6338e77f',
+        '7f5a2fb5-e3af-4395-9f38-c7c23dd8bec0',
+        '2021-03-19',
+        'Accommodation',
+        '''Home Trust Ltd. offers support with sourcing and/or sustaining appropriate accommodation. We offer a range of interventions to help with all accommodation-related support needs.
+
+We offer face-to-face advice, guidance and support to adult males (incl. young adult males), to secure and/or maintain suitable, settled accommodation (for a minimum 3-month period). This service is available to those who will be released from custody within the next 12 weeks and those already on licence in the community.
+
+Each referred client will be allocated a case worker who will work closely with them and their Probation Officer to assess needs and barriers and agree a detailed, individualised action plan.
+
+Interventions may include:
+  • liaising with landlords to sustain existing tenancies
+  • referrals to housing providers and/or the local authority
+  • support to access the private rented sector
+  • building independence through the delivery of group work and courses (e.g. how to be a good tenant)''',
+    ),
+    (
+        '11f06a1d-da75-4ca3-bb39-b7e848fb7612',
+        '9653676a-a51f-48cf-8541-444af11fe18b',
+        '2021-03-19',
+        'Employment, Training & Education',
+        '''New Beginnings will provide ETE services to empower and motivate SUs to develop towards and take up education, training and employment.
+
+• Benefits advice, including Better Off Calculations, so people have a clear understanding of the impact of training/work on their benefits
+• Support offered face-to-face, digitally, one-to-one and in groups, dependent on need and risk
+• Access to vacancies already secured with employers for whom a criminal conviction is not a barrier to employment
+• Funded training opportunities e.g. short adult education units, qualifications, full apprenticeships
+• Careers advice, including selecting appropriate sectors reflecting offending type, differing methods of securing work inc use of technology to increase during and post-Lockdowns
+• Further advice on how/when to disclose convictions to potential employers
+• The opportunity receive support from people with lived experience''',
+    ),
 ]
 
 sp_user_ids_with_username_and_organization = [
@@ -236,7 +288,9 @@ languages, weights = zip(*[
 
 def sql_format(x):
     if type(x) is str:
-        return repr(x)
+        x = x.replace("'", "''")
+        x = x.replace('\n', "' || chr(10) || '")
+        return f"'{x}'"
     elif x is None:
         return 'null'
     elif type(x) is bool:
@@ -446,4 +500,3 @@ if __name__ == '__main__':
         """)
 
         f.write(sql_values(referral_desired_outcomes))
-
