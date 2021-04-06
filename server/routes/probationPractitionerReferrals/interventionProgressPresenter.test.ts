84d118d3348d6edc55cbd8288aebe297fa06e471
George Eaton <george@dxw.com>
2021-04-06 12:25:13 +0100

    Add PP InterventionProgressPresenter

---
+++ server/routes/probationPractitionerReferrals/interventionProgressPresenter.test.ts
@@ -0,0 +1,180 @@
+import InterventionProgressPresenter from './interventionProgressPresenter'
+import sentReferralFactory from '../../../testutils/factories/sentReferral'
+import serviceCategoryFactory from '../../../testutils/factories/serviceCategory'
+import serviceUserFactory from '../../../testutils/factories/deliusServiceUser'
+
+describe(InterventionProgressPresenter, () => {
+  describe('sessionTableRows', () => {
+    it('returns an empty list if there are no appointments', () => {
+      const referral = sentReferralFactory.build()
+      const serviceCategory = serviceCategoryFactory.build()
+      const serviceUser = serviceUserFactory.build()
+      const presenter = new InterventionProgressPresenter(referral, serviceCategory, serviceUser, [])
+
+      expect(presenter.sessionTableRows).toEqual([])
+    })
+
+    describe('when a session exists but an appointment has not yet been scheduled', () => {
+      it('populates the table with formatted session information, with the "Edit session details" link displayed', () => {
+        const referral = sentReferralFactory.build()
+        const serviceCategory = serviceCategoryFactory.build()
+        const serviceUser = serviceUserFactory.build()
+        const presenter = new InterventionProgressPresenter(referral, serviceCategory, serviceUser, [
+          {
+            sessionNumber: 1,
+            appointmentTime: null,
+            durationInMinutes: null,
+          },
+        ])
+        expect(presenter.sessionTableRows).toEqual([
+          {
+            sessionNumber: 1,
+            appointmentTime: '',
+            tagArgs: {
+              text: 'NOT SCHEDULED',
+              classes: 'govuk-tag--grey',
+            },
+            linkHtml: '',
+          },
+        ])
+      })
+    })
+
+    describe('when an appointment has been scheduled', () => {
+      it('populates the table with formatted session information, with the "Reschedule session" and "Give feedback" links displayed', () => {
+        const referral = sentReferralFactory.build()
+        const serviceCategory = serviceCategoryFactory.build()
+        const serviceUser = serviceUserFactory.build()
+        const presenter = new InterventionProgressPresenter(referral, serviceCategory, serviceUser, [
+          {
+            sessionNumber: 1,
+            appointmentTime: '2020-12-07T13:00:00.000000Z',
+            durationInMinutes: 120,
+          },
+        ])
+        expect(presenter.sessionTableRows).toEqual([
+          {
+            sessionNumber: 1,
+            appointmentTime: '07 Dec 2020, 13:00',
+            tagArgs: {
+              text: 'SCHEDULED',
+              classes: 'govuk-tag--blue',
+            },
+            linkHtml: '',
+          },
+        ])
+      })
+    })
+
+    describe('after the scheduled appointment', () => {
+      describe('when the service user attended the session or was late', () => {
+        it('populates the table with the "completed" status against that session and a link to view it', () => {
+          const referral = sentReferralFactory.build()
+          const serviceCategory = serviceCategoryFactory.build()
+          const serviceUser = serviceUserFactory.build()
+          const presenter = new InterventionProgressPresenter(referral, serviceCategory, serviceUser, [
+            {
+              sessionNumber: 1,
+              appointmentTime: null,
+              durationInMinutes: null,
+              attendance: {
+                attended: 'yes',
+              },
+            },
+            {
+              sessionNumber: 2,
+              appointmentTime: null,
+              durationInMinutes: null,
+              attendance: {
+                attended: 'late',
+              },
+            },
+          ])
+          expect(presenter.sessionTableRows).toEqual([
+            {
+              sessionNumber: 1,
+              appointmentTime: '',
+              tagArgs: {
+                text: 'COMPLETED',
+                classes: 'govuk-tag--green',
+              },
+              linkHtml: '<a class="govuk-link" href="#">View</a>',
+            },
+            {
+              sessionNumber: 2,
+              appointmentTime: '',
+              tagArgs: {
+                text: 'COMPLETED',
+                classes: 'govuk-tag--green',
+              },
+              linkHtml: '<a class="govuk-link" href="#">View</a>',
+            },
+          ])
+        })
+      })
+
+      describe('when the service did not attend the session', () => {
+        it('populates the table with the "failure to attend" status against that session and a link to view it', () => {
+          const referral = sentReferralFactory.build()
+          const serviceCategory = serviceCategoryFactory.build()
+          const serviceUser = serviceUserFactory.build()
+          const presenter = new InterventionProgressPresenter(referral, serviceCategory, serviceUser, [
+            {
+              sessionNumber: 1,
+              appointmentTime: null,
+              durationInMinutes: null,
+              attendance: {
+                attended: 'no',
+              },
+            },
+          ])
+          expect(presenter.sessionTableRows).toEqual([
+            {
+              sessionNumber: 1,
+              appointmentTime: '',
+              tagArgs: {
+                text: 'FAILURE TO ATTEND',
+                classes: 'govuk-tag--purple',
+              },
+              linkHtml: '<a class="govuk-link" href="#">View</a>',
+            },
+          ])
+        })
+      })
+    })
+  })
+
+  describe('text', () => {
+    describe('title', () => {
+      it('returns a title to be displayed', () => {
+        const referral = sentReferralFactory.build()
+        const serviceCategory = serviceCategoryFactory.build({ name: 'accommodation' })
+        const serviceUser = serviceUserFactory.build()
+        const presenter = new InterventionProgressPresenter(referral, serviceCategory, serviceUser, [])
+
+        expect(presenter.text).toMatchObject({
+          title: 'Accommodation progress',
+        })
+      })
+    })
+  })
+
+  describe('referralAssigned', () => {
+    it('returns false when the referral has no assignee', () => {
+      const referral = sentReferralFactory.unassigned().build()
+      const serviceCategory = serviceCategoryFactory.build()
+      const serviceUser = serviceUserFactory.build()
+      const presenter = new InterventionProgressPresenter(referral, serviceCategory, serviceUser, [])
+
+      expect(presenter.referralAssigned).toEqual(false)
+    })
+    it('returns true when the referral has an assignee', () => {
+      const referral = sentReferralFactory.assigned().build()
+      const serviceCategory = serviceCategoryFactory.build()
+      const serviceUser = serviceUserFactory.build()
+      const presenter = new InterventionProgressPresenter(referral, serviceCategory, serviceUser, [])
+
+      expect(presenter.referralAssigned).toEqual(true)
+    })
+  })
+})
