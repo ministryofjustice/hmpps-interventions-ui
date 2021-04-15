c8c93762b24ca590691a333b8d07c63495844a50
George Eaton <george@dxw.com>
2021-04-15 09:35:09 +0100

    Switch test logging to `warn` level
    Since switching to structured logging, the output has become a little
    verbose and there's way too much noise. I'm tempted to turn off logging
    on tests completely, but there might be some value in having the logs
    for debugging.
    
    There's still some logging going on when running tests now, but it's a
    lot less.

--- log.ts
+++ log.ts
@@ -2,7 +2,7 @@ import Logger from 'bunyan'
 import type { ResponseError } from 'superagent'
 import config from './server/config'
 
-const level = config.production ? 'warn' : 'debug'
+const level = config.production || config.testMode ? 'warn' : 'debug'
 
 function responseErrorSerializer(err: ResponseError) {
   const baseErr = Logger.stdSerializers.err(err)
