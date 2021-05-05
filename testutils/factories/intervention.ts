b9af1b9563e32c8073a46fd194595e8524abc25e
Lawrence Forooghian <lawrence@dxw.com>
2021-05-05 20:47:18 +0100

    An intervention can have multiple service categories
    As in 206c3f4, we’re adding this to support cohort interventions. An
    intervention’s serviceCategories represents the possible list of service
    categories that a probation practitioner can choose from when making a
    referral for that intervention.
    
    Also as in 206c3f4, we’re initially adding support for multiple service
    categories alongside the existing single service category, and then in
    IC-1594 we’ll migrate the existing single category code and then get rid
    of the serviceCategory property.
    
    This also fixes an issue with the original implementation that didn't
    correctly access the `sequence` property, meaning calling `toString` on
    it was returning an object without us realising.

--- testutils/factories/intervention.ts
+++ testutils/factories/intervention.ts
@@ -4,10 +4,13 @@ import serviceCategoryFactory from './serviceCategory'
 import serviceProviderFactory from './serviceProvider'
 import eligibilityFactory from './eligibility'
 
-export default Factory.define<Intervention>(sequence => ({
-  id: sequence.toString(),
-  title: 'Better solutions (anger management)',
-  description: `To provide service users with key tools and strategies to address issues of anger management and temper control and
+export default Factory.define<Intervention>(({ sequence }) => {
+  const serviceCategory = serviceCategoryFactory.build()
+
+  return {
+    id: sequence.toString(),
+    title: 'Better solutions (anger management)',
+    description: `To provide service users with key tools and strategies to address issues of anger management and temper control and
 explore the link between thoughts, emotions and behaviour. It provides the opportunity for service users to practice
 these strategies in a safe and closed environment.
 
@@ -16,14 +19,16 @@ The service will use the following methods:
 • Group therapy sessions
 • One-to-one coaching
 • Hypnotherapy`,
-  npsRegion: { id: 'B', name: 'North West' },
-  pccRegions: [
-    { id: 'cheshire', name: 'Cheshire' },
-    { id: 'cumbria', name: 'Cumbria' },
-    { id: 'lancashire', name: 'Lancashire' },
-    { id: 'merseyside', name: 'Merseyside' },
-  ],
-  serviceCategory: serviceCategoryFactory.build(),
-  serviceProvider: serviceProviderFactory.build(),
-  eligibility: eligibilityFactory.allAdults().build(),
-}))
+    npsRegion: { id: 'B', name: 'North West' },
+    pccRegions: [
+      { id: 'cheshire', name: 'Cheshire' },
+      { id: 'cumbria', name: 'Cumbria' },
+      { id: 'lancashire', name: 'Lancashire' },
+      { id: 'merseyside', name: 'Merseyside' },
+    ],
+    serviceCategory,
+    serviceCategories: [serviceCategory],
+    serviceProvider: serviceProviderFactory.build(),
+    eligibility: eligibilityFactory.allAdults().build(),
+  }
+})
