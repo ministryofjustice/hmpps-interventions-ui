43fe481c48832e8d923ab01aacabfed2862d1bb8
rajivmahal <rajiv.mahal@digital.justice.gov.uk>
2021-05-18 08:17:58 +0100

    IC-1718 Notify Delius of Submitted End Of Service Report

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/service/CommunityAPIService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/service/CommunityAPIService.kt
@@ -8,8 +8,11 @@ import org.springframework.web.util.UriComponentsBuilder
 import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.component.CommunityAPIClient
 import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.events.AppointmentEvent
 import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.events.AppointmentEventType.SESSION_FEEDBACK_RECORDED
+import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.events.EndOfServiceReportEvent
+import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.events.EndOfServiceReportEventType
 import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.events.ReferralEvent
 import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.events.ReferralEventType
+import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.jpa.entity.Referral
 import java.time.OffsetDateTime
 
 interface CommunityAPIService
@@ -94,6 +97,56 @@ class CommunityAPIReferralEventService(
 }
 
 @Service
+class CommunityAPIEndOfServiceReportEventService(
+  @Value("\${interventions-ui.baseurl}") private val interventionsUIBaseURL: String,
+  @Value("\${interventions-ui.locations.submit-end-of-service-report}") private val interventionsUIEndOfServiceReportLocation: String,
+  @Value("\${community-api.locations.notification-request}") private val communityAPINotificationLocation: String,
+  @Value("\${community-api.integration-context}") private val integrationContext: String,
+  private val communityAPIClient: CommunityAPIClient,
+) : ApplicationListener<EndOfServiceReportEvent>, CommunityAPIService {
+  companion object : KLogging()
+
+  override fun onApplicationEvent(event: EndOfServiceReportEvent) {
+    when (event.type) {
+      EndOfServiceReportEventType.SUBMITTED,
+      -> {
+        val url = UriComponentsBuilder.fromHttpUrl(interventionsUIBaseURL)
+          .path(interventionsUIEndOfServiceReportLocation)
+          .buildAndExpand(event.endOfServiceReport.id)
+          .toString()
+
+        postNotificationRequest(event, url)
+      }
+    }
+  }
+
+  private fun postNotificationRequest(event: EndOfServiceReportEvent, url: String) {
+
+    val referral = event.endOfServiceReport.referral
+
+    val request = NotificationCreateRequestDTO(
+      "ACC", // Fixme: Using only contract type Accommodation til contract type changes are in
+      referral.sentAt!!,
+      event.endOfServiceReport.submittedAt!!,
+      getNotes(referral, url),
+    )
+
+    val communityApiSentReferralPath = UriComponentsBuilder.fromPath(communityAPINotificationLocation)
+      .buildAndExpand(referral.serviceUserCRN, referral.relevantSentenceId!!, integrationContext)
+      .toString()
+
+    communityAPIClient.makeAsyncPostRequest(communityApiSentReferralPath, request)
+  }
+
+  private fun getNotes(referral: Referral, url: String): String {
+    val contractTypeDescription = "Accommodation" // Fixme: Using only contract type Accommodation til contract type changes are in
+    return "End of Service Report for Referral ${referral.referenceNumber} has been submitted\n" +
+      "Prime Provider is ${referral.intervention.dynamicFrameworkContract.primeProvider.name} for Contract Type ${contractTypeDescription}\n" +
+      "$url"
+  }
+}
+
+@Service
 class CommunityAPIAppointmentEventService(
   @Value("\${interventions-ui.baseurl}") private val interventionsUIBaseURL: String,
   @Value("\${interventions-ui.locations.session-feedback}") private val interventionsUISessionFeedbackLocation: String,
@@ -142,6 +195,13 @@ data class ReferralEndRequest(
   val sentenceId: Long,
   val endType: String,
   val notes: String,
+)
+
+data class NotificationCreateRequestDTO(
+  val contractType: String,
+  val referralStart: OffsetDateTime,
+  val contactDateTime: OffsetDateTime,
+  val notes: String
 )
 
 data class AppointmentOutcomeRequest(
