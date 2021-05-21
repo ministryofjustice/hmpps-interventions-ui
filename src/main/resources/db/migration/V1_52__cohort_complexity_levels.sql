324dba0a591256bf6794b53eeec270f0b7f3366e
Tom Myers <tom.myers@digital.justice.gov.uk>
2021-05-21 15:02:18 +0100

    update sql query to update existing complexity levels and remove old column

--- src/main/resources/db/migration/V1_52__cohort_complexity_levels.sql
+++ src/main/resources/db/migration/V1_52__cohort_complexity_levels.sql
@@ -6,3 +6,11 @@ create table referral_complexity_level_ids (
     constraint fk_referral_complexity_level_ids_complexity_level_ids_key foreign key (referral_id, complexity_level_ids_key) references referral_selected_service_category,
     primary key (referral_id, complexity_level_ids_key)
 );
+
+insert into referral_complexity_level_ids
+    select referral.id, complexity_level.id, service_category.id from referral, complexity_level, service_category
+    where referral.complexity_levelid = complexity_level.id
+    and complexity_level.service_category_id = service_category.id;
+
+alter table referral
+    drop column complexity_levelid;
