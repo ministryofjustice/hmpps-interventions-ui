0f7760c7ec6c85e3fe7e57ac780cddb0bbc20fe3
Lawrence Forooghian <lawrence@dxw.com>
2021-05-25 15:43:46 +0100

    Add needs and requirements to referral check-answers page
    This is a replay of the data displayed on the
    /referrals/:id/needs-and-requirements page. However, on that page the
    needsInterpreter/interpreterLanguage (and same for responsibilities) are
    displayed as two separate questions, but the designers have preferred to
    display them as a single concatenated answer here.

--- server/routes/referrals/checkAnswersPresenter.ts
+++ server/routes/referrals/checkAnswersPresenter.ts
@@ -2,6 +2,7 @@ import DraftReferral from '../../models/draftReferral'
 import ServiceCategory from '../../models/serviceCategory'
 import { SummaryListItem } from '../../utils/summaryList'
 import ServiceUserDetailsPresenter from './serviceUserDetailsPresenter'
+import NeedsAndRequirementsPresenter from './needsAndRequirementsPresenter'
 
 export default class CheckAnswersPresenter {
   constructor(private readonly referral: DraftReferral, private readonly serviceCategory: ServiceCategory) {}
@@ -11,6 +12,49 @@ export default class CheckAnswersPresenter {
       title: `${this.serviceUserName}’s personal details`,
       summary: new ServiceUserDetailsPresenter(this.referral.serviceUser).summary,
     }
+  }
+
+  get needsAndRequirementsSection(): { title: string; summary: SummaryListItem[] } {
+    const needsAndRequirementsPresenter = new NeedsAndRequirementsPresenter(this.referral)
+
+    return {
+      title: `${this.serviceUserName}’s needs and requirements`,
+      summary: [
+        {
+          key: needsAndRequirementsPresenter.text.additionalNeedsInformation.label,
+          lines: [needsAndRequirementsPresenter.fields.additionalNeedsInformation],
+        },
+        {
+          key: needsAndRequirementsPresenter.text.accessibilityNeeds.label,
+          lines: [needsAndRequirementsPresenter.fields.accessibilityNeeds],
+        },
+        {
+          key: needsAndRequirementsPresenter.text.needsInterpreter.label,
+          lines: [
+            this.conditionalValue(
+              needsAndRequirementsPresenter.fields.needsInterpreter,
+              needsAndRequirementsPresenter.fields.interpreterLanguage
+            ),
+          ],
+        },
+        {
+          key: needsAndRequirementsPresenter.text.hasAdditionalResponsibilities.label,
+          lines: [
+            this.conditionalValue(
+              needsAndRequirementsPresenter.fields.hasAdditionalResponsibilities,
+              needsAndRequirementsPresenter.fields.whenUnavailable
+            ),
+          ],
+        },
+      ],
+    }
+  }
+
+  private conditionalValue(isSelected: boolean | null, dependentAnswerText: string) {
+    if (isSelected) {
+      return `Yes. ${dependentAnswerText}`
+    }
+    return 'No'
   }
 
   private readonly serviceUserName = this.referral.serviceUser?.firstName ?? ''
