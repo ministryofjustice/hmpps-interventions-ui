f7e0f95a175ae544995a765b1fb877d80d41ceab
David Lantos <david.lantos@gmail.com>
2021-05-27 00:43:49 +0100

    Implement intervention end reason field for NDMIS

--- helm_deploy/hmpps-interventions-service/reports/crs_performance_report.sql
+++ helm_deploy/hmpps-interventions-service/reports/crs_performance_report.sql
@@ -1,4 +1,10 @@
 COPY (
+  WITH attended_sessions AS (
+      select count(action_plan_id) AS attended, action_plan_id
+      from action_plan_appointment
+      where attended in ('YES', 'LATE')
+      group by action_plan_id
+  )
   SELECT
     r.reference_number      AS referral_ref,
     r.id                    AS referral_id,
@@ -25,12 +31,16 @@ COPY (
     )                       AS outcomes_to_be_achieved_count,
     'coming-later'          AS outcomes_progress,
     ap.number_of_sessions   AS count_of_sessions_expected,
-    (
-      select count(app.id)
-      from action_plan_appointment app
-      where app.action_plan_id = ap.id and attended in ('YES', 'LATE')
-    )                       AS count_of_sessions_attended,
+    s.attended              AS count_of_sessions_attended,
     r.concluded_at          AS date_intervention_ended,
+    (
+      CASE
+        WHEN r.concluded_at IS NOT NULL AND eosr.id IS NULL THEN 'cancelled'
+        WHEN r.concluded_at IS NOT NULL AND eosr.id IS NOT NULL AND ap.number_of_sessions > s.attended THEN 'ended'
+        WHEN r.concluded_at IS NOT NULL AND eosr.id IS NOT NULL AND ap.number_of_sessions = s.attended THEN 'completed'
+        ELSE NULL
+      END
+    )                       AS intervention_end_reason,
     eosr.submitted_at       AS date_end_of_service_report_submitted
   FROM
     referral r
@@ -39,6 +49,7 @@ COPY (
     JOIN contract_type ct ON (c.contract_type_id = ct.id)
     JOIN service_provider prime ON (c.prime_provider_id = prime.id)
     LEFT JOIN action_plan ap ON (ap.referral_id = r.id) --❗️assumes a SINGLE action plan
+    LEFT JOIN attended_sessions s ON (s.action_plan_id = ap.id) --❗️should be linked to referrals instead, sessions are static
     LEFT JOIN end_of_service_report eosr ON (eosr.referral_id = r.id)
   WHERE
     r.sent_at IS NOT NULL
