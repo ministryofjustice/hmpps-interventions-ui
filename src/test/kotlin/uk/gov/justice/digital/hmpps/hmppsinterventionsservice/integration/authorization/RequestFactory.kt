7d7941ebf011bb333f8951d6c61db1c5d599f41e
Tom Myers <tom.myers@digital.justice.gov.uk>
2021-06-02 14:29:37 +0100

    add new integration tests for PP referral access and creation

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/integration/authorization/RequestFactory.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/integration/authorization/RequestFactory.kt
@@ -22,17 +22,25 @@ enum class Request {
 }
 
 class RequestFactory(private val webTestClient: WebTestClient, private val setupAssistant: SetupAssistant) {
-  fun create(request: Request, token: String?, vararg urlParams: String): WebTestClient.RequestHeadersSpec<*> {
+  fun create(request: Request, token: String?, vararg urlParams: String, body: Any? = null): WebTestClient.RequestHeadersSpec<*> {
     val spec = when (request) {
-      Request.CreateDraftReferral -> webTestClient.post().uri("/draft-referral").bodyValue(CreateReferralRequestDTO("X123456", UUID.randomUUID()))
+      Request.CreateDraftReferral -> webTestClient.post().uri("/draft-referral").bodyValue(
+        if (body != null) body as CreateReferralRequestDTO else CreateReferralRequestDTO("X123456", UUID.randomUUID())
+      )
 
       Request.GetDraftReferral -> webTestClient.get().uri("/draft-referral/${urlParams[0]}")
-      Request.UpdateDraftReferral -> webTestClient.patch().uri("/draft-referral/${urlParams[0]}").bodyValue(DraftReferralDTO())
+      Request.UpdateDraftReferral -> webTestClient.patch().uri("/draft-referral/${urlParams[0]}").bodyValue(
+        if (body != null) body as DraftReferralDTO else DraftReferralDTO()
+      )
       Request.SendDraftReferral -> webTestClient.post().uri("/draft-referral/${urlParams[0]}/send")
 
       Request.GetSentReferral -> webTestClient.get().uri("/sent-referral/${urlParams[0]}")
-      Request.AssignSentReferral -> webTestClient.post().uri("/sent-referral/${urlParams[0]}/assign").bodyValue(ReferralAssignmentDTO(AuthUserDTO.from(setupAssistant.createSPUser())))
-      Request.EndSentReferral -> webTestClient.post().uri("/sent-referral/${urlParams[0]}/end").bodyValue(EndReferralRequestDTO(setupAssistant.randomCancellationReason().code, "comments"))
+      Request.AssignSentReferral -> webTestClient.post().uri("/sent-referral/${urlParams[0]}/assign").bodyValue(
+        if (body != null) body as ReferralAssignmentDTO else ReferralAssignmentDTO(AuthUserDTO.from(setupAssistant.createSPUser()))
+      )
+      Request.EndSentReferral -> webTestClient.post().uri("/sent-referral/${urlParams[0]}/end").bodyValue(
+        if (body != null) body as EndReferralRequestDTO else EndReferralRequestDTO(setupAssistant.randomCancellationReason().code, "comments")
+      )
 
       Request.GetDraftReferrals -> webTestClient.get().uri("/draft-referrals")
       Request.GetSentReferrals -> webTestClient.get().uri("/sent-referrals")
