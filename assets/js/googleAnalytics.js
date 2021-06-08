0c1abd8b05c7eeb3308468ff8b5611dced227046
John Nolan <john.nolan@digital.justice.gov.uk>
2021-06-08 12:37:53 +0100

    Adds google analytics script with placeholder for dynamic analytics id

---
+++ assets/js/googleAnalytics.js
@@ -0,0 +1,21 @@
+const analyticsId = window.gaConfig.uaId;
+let s = document.createElement('script');
+s.type = 'text/javascript';
+s.src = 'https://www.googletagmanager.com/gtag/js?id=' + analyticsId;
+document.getElementsByTagName('head')[0].appendChild(s);
+window.dataLayer = window.dataLayer || [];
+window.gtag = function () {
+    window.dataLayer.push(arguments);
+}
+window.gtag('js', new Date());
+window.gtag('config', analyticsId, {
+    'linker': {
+        'domains': ['www.gov.uk']
+    },
+    'transport_type': 'beacon',
+    'anonymize_ip': true, // https://developers.google.com/analytics/devguides/collection/gtagjs/ip-anonymization
+    'allow_google_signals': false, // https://developers.google.com/analytics/devguides/collection/gtagjs/display-features
+    'allow_ad_personalization_signals': false, // https://developers.google.com/analytics/devguides/collection/gtagjs/display-features
+    'page_title': document.title,
+    'page_path': location.pathname.split('?')[0]
+});
