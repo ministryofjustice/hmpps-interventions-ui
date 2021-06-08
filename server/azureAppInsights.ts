15c366b43fdab1a1fcac9f78c37a4c0bb33018fa
Tom Myers <tom.myers@digital.justice.gov.uk>
2021-06-08 01:17:30 +0100

    IC-1867 ignore requests for static assets in application insights

--- server/azureAppInsights.ts
+++ server/azureAppInsights.ts
@@ -1,4 +1,4 @@
-import { setup, defaultClient, DistributedTracingModes } from 'applicationinsights'
+import { setup, Contracts, defaultClient, DistributedTracingModes } from 'applicationinsights'
 import applicationVersion from './applicationVersion'
 import config from './config'
 import logger from '../log'
@@ -15,6 +15,19 @@ function version(): string {
   return buildNumber
 }
 
+function ignoreStaticAssetsProcessor(
+  envelope: Contracts.EnvelopeTelemetry,
+  _contextObjects: { [name: string]: unknown } | undefined
+): boolean {
+  if (envelope.data.baseType === Contracts.TelemetryTypeString.Request) {
+    const requestData = envelope.data.baseData
+    if (requestData instanceof Contracts.RequestData && requestData.name.startsWith('GET /assets/')) {
+      return false
+    }
+  }
+  return true
+}
+
 export default function initialiseAppInsights(): void {
   const { connectionString } = config.applicationInsights
   if (connectionString !== null) {
@@ -22,7 +35,11 @@ export default function initialiseAppInsights(): void {
 
     setup(connectionString).setDistributedTracingMode(DistributedTracingModes.AI_AND_W3C).start()
 
+    // application level properties
     defaultClient.context.tags['ai.cloud.role'] = defaultName()
     defaultClient.context.tags['ai.application.ver'] = version()
+
+    // custom processors to fine tune behaviour
+    defaultClient.addTelemetryProcessor(ignoreStaticAssetsProcessor)
   }
 }
