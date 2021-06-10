8cbe7b8a362f3e636e8d76b3cf5d3c7b4e4c5e39
Tom Myers <tom.myers@digital.justice.gov.uk>
2021-06-10 17:09:37 +0100

    update ActionPlanSessionFactory to be able to easily make unscheduled, scheduled, or attended sessions

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/util/ActionPlanSessionFactory.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/util/ActionPlanSessionFactory.kt
@@ -3,6 +3,7 @@ package uk.gov.justice.digital.hmpps.hmppsinterventionsservice.util
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager
 import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.jpa.entity.ActionPlan
 import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.jpa.entity.ActionPlanSession
+import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.jpa.entity.Appointment
 import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.jpa.entity.Attended
 import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.jpa.entity.AuthUser
 import java.time.OffsetDateTime
@@ -11,30 +12,49 @@ import java.util.UUID
 class ActionPlanSessionFactory(em: TestEntityManager? = null) : EntityFactory(em) {
   private val actionPlanFactory = ActionPlanFactory(em)
 
-  fun create(
+  fun createUnscheduled(
     id: UUID = UUID.randomUUID(),
     actionPlan: ActionPlan = actionPlanFactory.create(),
     sessionNumber: Int = 1,
-    createdAt: OffsetDateTime = OffsetDateTime.now(),
-    createdBy: AuthUser = actionPlan.createdBy,
-    attended: Attended? = null,
-    additionalAttendanceInformation: String? = null,
-    attendanceSubmittedAt: OffsetDateTime? = null,
-    notifyPPOfAttendanceBehaviour: Boolean? = null,
-    sessionFeedbackSubmittedAt: OffsetDateTime? = null,
   ): ActionPlanSession {
     return save(
       ActionPlanSession(
         id = id,
         actionPlan = actionPlan,
         sessionNumber = sessionNumber,
-        createdAt = createdAt,
+        appointments = mutableSetOf()
+      )
+    )
+  }
+
+  fun createScheduled(
+    id: UUID = UUID.randomUUID(),
+    actionPlan: ActionPlan = actionPlanFactory.create(),
+    sessionNumber: Int = 1,
+    createdAt: OffsetDateTime = OffsetDateTime.now(),
+    createdBy: AuthUser = actionPlan.createdBy,
+    appointmentTime: OffsetDateTime = OffsetDateTime.now().plusMonths(1),
+    durationInMinutes: Int = 120,
+    deliusAppointmentId: Long? = null,
+  ): ActionPlanSession {
+    val appointment = save(
+      Appointment(
+        id = UUID.randomUUID(),
         createdBy = createdBy,
-        attended = attended,
-        additionalAttendanceInformation = additionalAttendanceInformation,
-        attendanceSubmittedAt = attendanceSubmittedAt,
-        notifyPPOfAttendanceBehaviour = notifyPPOfAttendanceBehaviour,
-        sessionFeedbackSubmittedAt = sessionFeedbackSubmittedAt,
+        createdAt = createdAt,
+        appointmentTime = appointmentTime,
+        durationInMinutes = durationInMinutes,
+        appointmentFeedbackSubmittedBy = createdBy,
+        deliusAppointmentId = deliusAppointmentId,
+      )
+    )
+
+    return save(
+      ActionPlanSession(
+        id = id,
+        actionPlan = actionPlan,
+        sessionNumber = sessionNumber,
+        appointments = mutableSetOf(appointment)
       )
     )
   }
@@ -45,22 +65,39 @@ class ActionPlanSessionFactory(em: TestEntityManager? = null) : EntityFactory(em
     sessionNumber: Int = 1,
     createdAt: OffsetDateTime = OffsetDateTime.now(),
     createdBy: AuthUser = actionPlan.createdBy,
+    appointmentTime: OffsetDateTime = OffsetDateTime.now().plusMonths(1),
+    durationInMinutes: Int = 120,
+    deliusAppointmentId: Long? = null,
     attended: Attended? = Attended.YES,
     attendanceSubmittedAt: OffsetDateTime? = createdAt,
+    additionalAttendanceInformation: String? = null,
     notifyPPOfAttendanceBehaviour: Boolean? = false,
-    sessionFeedbackSubmittedAt: OffsetDateTime? = attendanceSubmittedAt,
+    appointmentFeedbackSubmittedAt: OffsetDateTime? = attendanceSubmittedAt,
+    appointmentFeedbackSubmittedBy: AuthUser? = createdBy,
   ): ActionPlanSession {
+    val appointment = save(
+      Appointment(
+        id = UUID.randomUUID(),
+        createdBy = createdBy,
+        createdAt = createdAt,
+        appointmentTime = appointmentTime,
+        durationInMinutes = durationInMinutes,
+        appointmentFeedbackSubmittedBy = createdBy,
+        deliusAppointmentId = deliusAppointmentId,
+        attended = attended,
+        attendanceSubmittedAt = attendanceSubmittedAt,
+        additionalAttendanceInformation = additionalAttendanceInformation,
+        notifyPPOfAttendanceBehaviour = notifyPPOfAttendanceBehaviour,
+        appointmentFeedbackSubmittedAt = appointmentFeedbackSubmittedAt,
+      )
+    )
+
     return save(
       ActionPlanSession(
         id = id,
         actionPlan = actionPlan,
         sessionNumber = sessionNumber,
-        createdAt = createdAt,
-        createdBy = createdBy,
-        attended = attended,
-        attendanceSubmittedAt = attendanceSubmittedAt,
-        notifyPPOfAttendanceBehaviour = notifyPPOfAttendanceBehaviour,
-        sessionFeedbackSubmittedAt = sessionFeedbackSubmittedAt,
+        appointments = mutableSetOf(appointment)
       )
     )
   }
