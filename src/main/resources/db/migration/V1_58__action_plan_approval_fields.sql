247b42e66d876b59a9483d054bf894bc5e735690
Tom Myers <tom.myers@digital.justice.gov.uk>
2021-06-14 10:57:37 +0100

    add unique contraint to action_plan_session table to ensure each numbered session can only be created once

--- src/main/resources/db/migration/V1_58__action_plan_approval_fields.sql
+++ src/main/resources/db/migration/V1_58__action_plan_approval_fields.sql
@@ -2,3 +2,6 @@ alter table action_plan
     add column approved_at timestamp with time zone,
     add column approved_by_id text,
     add constraint fk_action_plan_approved_by foreign key (approved_by_id) references auth_user;
+
+alter table action_plan_session
+    add unique (action_plan_id, session_number);
