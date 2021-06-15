8191bdb85148adee0f65ca0ec78411b74698ae94
ryanforsyth <ryan.forsyth@digital.justice.gov.uk>
2021-06-15 15:57:35 +0100

    make table name and columns singular & add supplier assessment appointment dev data

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/jpa/entity/SupplierAssessment.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/jpa/entity/SupplierAssessment.kt
@@ -5,6 +5,8 @@ import org.hibernate.annotations.FetchMode
 import java.util.UUID
 import javax.persistence.Entity
 import javax.persistence.Id
+import javax.persistence.JoinColumn
+import javax.persistence.JoinTable
 import javax.persistence.OneToMany
 import javax.persistence.OneToOne
 import javax.persistence.Table
@@ -16,8 +18,13 @@ data class SupplierAssessment(
   @Id val id: UUID,
   @NotNull @OneToOne val referral: Referral,
 
+  @JoinTable(
+    name = "supplier_assessment_appointment",
+    joinColumns = [JoinColumn(name = "supplier_assessment_id")],
+    inverseJoinColumns = [JoinColumn(name = "appointment_id")]
+  )
   @NotNull @OneToMany @Fetch(FetchMode.JOIN) val appointments: MutableSet<Appointment> = mutableSetOf(),
 ) {
-  val appointment: Appointment
+  val currentAppointment: Appointment
     get() = appointments.maxByOrNull { it.createdAt }!!
 }
