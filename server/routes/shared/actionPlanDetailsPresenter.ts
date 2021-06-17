8ffcccb9aa5422af9d0d2413f1632aeae7a80eb7
Tom Myers <tom.myers@digital.justice.gov.uk>
2021-06-17 10:02:58 +0100

    fix creation of action plans by ensuring the referral ID is always valid

--- server/routes/shared/actionPlanDetailsPresenter.ts
+++ server/routes/shared/actionPlanDetailsPresenter.ts
@@ -1,19 +1,21 @@
 import ActionPlan from '../../models/actionPlan'
 import DateUtils from '../../utils/dateUtils'
+import SentReferral from '../../models/sentReferral'
 
 export default class ActionPlanDetailsPresenter {
   constructor(
+    private readonly referral: SentReferral,
     private readonly actionPlan: ActionPlan | null,
     private readonly subNavUrlPrefix: 'service-provider' | 'probation-practitioner'
   ) {}
 
-  readonly interventionProgressURL = `/${this.subNavUrlPrefix}/referrals/${this.actionPlan?.referralId}/progress`
+  readonly interventionProgressURL = `/${this.subNavUrlPrefix}/referrals/${this.referral.id}/progress`
+
+  readonly createActionPlanFormAction = `/service-provider/referrals/${this.referral.id}/action-plan`
+
+  readonly viewActionPlanUrl = `/service-provider/referrals/${this.referral.id}/action-plan`
 
   readonly actionPlanFormUrl = `/service-provider/action-plan/${this.actionPlan?.id}/add-activities`
-
-  readonly createActionPlanFormAction = `/service-provider/referrals/${this.actionPlan?.referralId}/action-plan`
-
-  readonly viewActionPlanUrl = `/service-provider/referrals/${this.actionPlan?.referralId}/action-plan`
 
   readonly text = {
     actionPlanStatus: this.actionPlanStatus,
