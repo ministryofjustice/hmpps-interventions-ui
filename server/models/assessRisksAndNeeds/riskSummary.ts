cb6a2dfc2c459a1282a3116e73f36b06e3a93041
Tom Myers <tom.myers@digital.justice.gov.uk>
2021-06-25 11:53:54 +0100

    add risk to self information into RiskSummary model

--- server/models/assessRisksAndNeeds/riskSummary.ts
+++ server/models/assessRisksAndNeeds/riskSummary.ts
@@ -1,6 +1,21 @@
 export type RiskScore = 'LOW' | 'MEDIUM' | 'HIGH' | 'VERY_HIGH'
 
+export type RiskResponse = 'YES' | 'NO' | 'DK'
+
+export interface Risk {
+  risk: RiskResponse | null
+  current: RiskResponse | null
+  currentConcernsText: string | null
+}
+
 export default interface RiskSummary {
+  riskToSelf: {
+    suicide?: Risk | null
+    selfHarm?: Risk | null
+    custody?: Risk | null
+    hostelSetting?: Risk | null
+    vulnerability?: Risk | null
+  }
   summary: {
     whoIsAtRisk?: string | null
     natureOfRisk?: string | null
