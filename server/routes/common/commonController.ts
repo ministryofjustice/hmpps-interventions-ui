2b237b9f67c4e35e849df72e63e1349fbb486047
Tom Myers <tom.myers@digital.justice.gov.uk>
2021-06-28 17:26:14 +0100

    update report a problem page to contain purely static content
    since the content is no longer dependent on the user type, i removed the view
    and presenter to reduce complexity and just have all the html for this page
    in the template.

--- server/routes/common/commonController.ts
+++ server/routes/common/commonController.ts
@@ -1,13 +1,8 @@
 import { Request, Response } from 'express'
-import ReportAProblemPresenter from './reportAProblemPresenter'
-import ReportAProblemView from './reportAProblemView'
 import ControllerUtils from '../../utils/controllerUtils'
 
 export default class CommonController {
   async reportAProblem(req: Request, res: Response): Promise<void> {
-    const { authSource } = res.locals.user
-    const presenter = new ReportAProblemPresenter(authSource === 'delius')
-    const view = new ReportAProblemView(presenter)
-    ControllerUtils.renderWithLayout(res, view, null)
+    ControllerUtils.renderWithLayout(res, { renderArgs: ['common/reportAProblem', {}] }, null)
   }
 }
--- server/routes/common/reportAProblemPresenter.ts
+++
@@ -1,7 +0,0 @@
-export default class ReportAProblemPresenter {
-  constructor(public readonly ppUser: boolean) {}
-
-  get email(): string {
-    return 'refer-and-monitor-feedback@digital.justice.gov.uk'
-  }
-}
