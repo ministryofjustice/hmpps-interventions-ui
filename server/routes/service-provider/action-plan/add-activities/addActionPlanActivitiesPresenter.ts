b534a72fe04059d720f887fb4dc924ea9082977e
Tom Myers <tom.myers@digital.justice.gov.uk>
2021-06-30 22:34:34 +0100

    remove use of repeated code in 'AddActionPlanActivityPresenter'

--- server/routes/service-provider/action-plan/add-activities/addActionPlanActivitiesPresenter.ts
+++ server/routes/service-provider/action-plan/add-activities/addActionPlanActivitiesPresenter.ts
@@ -3,25 +3,25 @@ import SentReferral from '../../../../models/sentReferral'
 import ServiceCategory from '../../../../models/serviceCategory'
 import { FormValidationError } from '../../../../utils/formValidationError'
 import PresenterUtils from '../../../../utils/presenterUtils'
-import utils from '../../../../utils/utils'
+import ActionPlanPresenter from '../../../shared/action-plan/actionPlanPresenter'
 
 export default class AddActionPlanActivitiesPresenter {
+  actionPlanPresenter: ActionPlanPresenter
+
   constructor(
     private readonly sentReferral: SentReferral,
     private readonly serviceCategories: ServiceCategory[],
     private readonly actionPlan: ActionPlan,
     private readonly errors: FormValidationError | null = null
-  ) {}
+  ) {
+    this.actionPlanPresenter = new ActionPlanPresenter(sentReferral, actionPlan, serviceCategories, 'service-provider')
+  }
 
   readonly saveAndContinueFormAction = `/service-provider/action-plan/${this.actionPlan.id}/add-activities`
 
   readonly addActivityAction = `/service-provider/action-plan/${this.actionPlan.id}/add-activity`
 
   readonly activityNumber = this.actionPlan.activities.length + 1
-
-  private readonly desiredOutcomesIds = this.sentReferral.referral.desiredOutcomes.flatMap(
-    desiredOutcome => desiredOutcome.desiredOutcomesIds
-  )
 
   readonly errorMessage = PresenterUtils.errorMessage(this.errors, 'description')
 
@@ -31,15 +31,4 @@ export default class AddActionPlanActivitiesPresenter {
     title: `Add activity ${this.activityNumber} to action plan`,
     referredOutcomesHeader: `Referred outcomes for ${this.sentReferral.referral.serviceUser.firstName}`,
   }
-
-  readonly desiredOutcomesByServiceCategory = this.serviceCategories.map(serviceCategory => {
-    const desiredOutcomesForServiceCategory = serviceCategory.desiredOutcomes.filter(desiredOutcome =>
-      this.desiredOutcomesIds.includes(desiredOutcome.id)
-    )
-
-    return {
-      serviceCategory: utils.convertToProperCase(serviceCategory.name),
-      desiredOutcomes: desiredOutcomesForServiceCategory.map(desiredOutcome => desiredOutcome.description),
-    }
-  })
 }
