77a3058d8164b20b1128e7e3abf752160b54668b
Tom Myers <tom.myers@digital.justice.gov.uk>
2021-07-06 11:11:45 +0100

    combine first submitted at and first approved at subqueries to improve performance

--- helm_deploy/hmpps-interventions-service/reports/crs_performance_report.sql
+++ helm_deploy/hmpps-interventions-service/reports/crs_performance_report.sql
@@ -15,15 +15,10 @@ COPY (
       where attended IS NOT NULL
       group by aps.action_plan_id
   ),
-  first_submitted_action_plans AS (
-    select referral_id, min(submitted_at) as submitted_at
+  first_submitted_and_approved_action_plans AS (
+    select referral_id, min(submitted_at) as first_submitted_at, min(approved_at) as first_approved_at
     from action_plan
     group by referral_id
-  ),
-  first_approved_action_plans AS (
-     select referral_id, min(approved_at) as approved_at
-     from action_plan
-     group by referral_id
   ),
   current_action_plans AS (
       select id, referral_id, number_of_sessions from action_plan
@@ -46,8 +41,8 @@ COPY (
     r.sent_at               AS date_referral_received,
     TIMESTAMP WITH TIME ZONE '3000-01-01+00' AS date_saa_booked,                    -- default value, coming later
     TIMESTAMP WITH TIME ZONE '3000-01-01+00' AS date_saa_attended,                  -- default value, coming later
-    fsap.submitted_at       AS date_first_action_plan_submitted,
-    faap.approved_at        AS date_of_first_action_plan_approval,
+    fsaap.first_submitted_at       AS date_first_action_plan_submitted,
+    fsaap.first_approved_at        AS date_of_first_action_plan_approval,
     shows.first_appointment AS date_of_first_session,
     (
       select count(o.desired_outcome_id)
@@ -74,8 +69,7 @@ COPY (
     JOIN dynamic_framework_contract c ON (i.dynamic_framework_contract_id = c.id)
     JOIN contract_type ct ON (c.contract_type_id = ct.id)
     JOIN service_provider prime ON (c.prime_provider_id = prime.id)
-    LEFT JOIN first_submitted_action_plans fsap ON (fsap.referral_id = r.id)
-    LEFT JOIN first_approved_action_plans faap ON (faap.referral_id = r.id)
+    LEFT JOIN first_submitted_and_approved_action_plans fsaap ON (fsaap.referral_id = r.id)
     LEFT JOIN current_action_plans cap ON (cap.referral_id = r.id)
     LEFT JOIN attended_sessions shows ON (shows.action_plan_id = cap.id) --❗️should be linked to referrals instead, sessions are static
     LEFT JOIN attempted_sessions atts ON (atts.action_plan_id = cap.id) --❗️should be linked to referrals instead, sessions are static
