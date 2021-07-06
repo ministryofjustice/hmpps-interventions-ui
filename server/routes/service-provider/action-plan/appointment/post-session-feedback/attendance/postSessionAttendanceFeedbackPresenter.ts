8d6fb45ca0ea7dfb8f2f5f1b246756a66de7eecc
Andrew Burrows <andrew.burrows@digital.justice.gov.uk>
2021-07-06 10:50:58 +0100

    IC-2048: Add appointment meeting type to attendence question for session feedback

--- server/routes/service-provider/action-plan/appointment/post-session-feedback/attendance/postSessionAttendanceFeedbackPresenter.ts
+++ server/routes/service-provider/action-plan/appointment/post-session-feedback/attendance/postSessionAttendanceFeedbackPresenter.ts
@@ -16,9 +16,23 @@ export default class PostSessionAttendanceFeedbackPresenter {
   readonly text = {
     title: `Add attendance feedback`,
     subTitle: 'Session details',
-    attendanceQuestion: `Did ${this.serviceUser.firstName} attend this session?`,
+    attendanceQuestion: this.attendanceQuestion,
     attendanceQuestionHint: 'Select one option',
     additionalAttendanceInformationLabel: `Add additional information about ${this.serviceUser.firstName}'s attendance:`,
+  }
+
+  get attendanceQuestion(): string {
+    switch (this.appointment.appointmentDeliveryType) {
+      case 'PHONE_CALL':
+        return `Did ${this.serviceUser.firstName} join this phone call?`
+      case 'VIDEO_CALL':
+        return `Did ${this.serviceUser.firstName} join this video call?`
+      case 'IN_PERSON_MEETING_OTHER':
+      case 'IN_PERSON_MEETING_PROBATION_OFFICE':
+        return `Did ${this.serviceUser.firstName} attend this in-person meeting?`
+      default:
+        return `Did ${this.serviceUser.firstName} attend this session?`
+    }
   }
 
   readonly errorSummary = PresenterUtils.errorSummary(this.error)
