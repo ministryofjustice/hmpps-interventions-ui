e1e3343db43c9814baf77c9ee7cf2adddbda2a4a
Tom Myers <tom.myers@digital.justice.gov.uk>
2021-08-09 14:38:38 +0100

    add 'approvedActionPlan' getter to referral entity

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/jpa/entity/ReferralTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/jpa/entity/ReferralTest.kt
@@ -3,13 +3,16 @@ package uk.gov.justice.digital.hmpps.hmppsinterventionsservice.jpa.entity
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.util.ActionPlanFactory
 import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.util.AuthUserFactory
 import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.util.ReferralFactory
 import java.time.OffsetDateTime
+import java.util.UUID
 
 internal class ReferralTest {
   private val referralFactory = ReferralFactory()
   private val authUserFactory = AuthUserFactory()
+  private val actionPlanFactory = ActionPlanFactory()
 
   @Nested
   inner class Assignments {
@@ -39,6 +42,32 @@ internal class ReferralTest {
       )
 
       assertThat(referral.currentAssignee).isEqualTo(currentAssignee)
+    }
+  }
+
+  @Nested inner class ActionPlans {
+    @Test
+    fun `approvedActionPlan returns null if there are no approved action plan`() {
+      val referral = referralFactory.createSent(
+        actionPlans = mutableListOf(
+          actionPlanFactory.createSubmitted()
+        )
+      )
+
+      assertThat(referral.approvedActionPlan).isNull()
+    }
+
+    @Test
+    fun `approvedActionPlan returns most recent approved action plan`() {
+      val correctId = UUID.randomUUID()
+      val referral = referralFactory.createSent(
+        actionPlans = mutableListOf(
+          actionPlanFactory.createSubmitted(),
+          actionPlanFactory.createApproved(createdAt = OffsetDateTime.now().minusHours(1)),
+          actionPlanFactory.createApproved(createdAt = OffsetDateTime.now(), id = correctId)
+        )
+      )
+      assertThat(referral.approvedActionPlan?.id).isEqualTo(correctId)
     }
   }
 }
