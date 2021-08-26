de55fc897d1b696414659e50d2897054120ebeeb
Lawrence Forooghian <lawrence@dxw.com>
2021-08-26 11:23:56 +0100

    Add a delivery schedule page
    A new static page that gives the user an outline of the delivery
    schedule for the next couple of months. Linked to from the footer.

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -47,6 +47,10 @@ export default function routes(router: Router, services: Services): Router {
     return commonController.reportAProblem(req, res)
   })
 
+  get(router, '/delivery-schedule', (req, res) => {
+    return commonController.deliverySchedule(req, res)
+  })
+
   if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test') {
     get(router, '/static-pages', (req, res) => {
       return staticContentController.index(req, res)
