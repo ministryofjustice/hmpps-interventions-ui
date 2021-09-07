90cc7ba5fecf789741e6ab257aab37d98c1be292
Andrew Burrows <andrew.burrows@digital.justice.gov.uk>
2021-09-07 12:47:56 +0100

    Added GET /case-note end point calling to interventions-service. This provides ability to get a case note for a referral from the case note id.

--- server/services/interventionsService.ts
+++ server/services/interventionsService.ts
@@ -621,4 +621,12 @@ export default class InterventionsService {
       data: createCaseNoteDTO,
     })) as CaseNote
   }
+
+  async getCaseNote(token: string, caseNoteId: string): Promise<CaseNote> {
+    const restClient = this.createRestClient(token)
+    return (await restClient.get({
+      path: `/case-note/${caseNoteId}`,
+      headers: { Accept: 'application/json' },
+    })) as CaseNote
+  }
 }
