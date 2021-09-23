72b6e3ad1dbc0821e33feb2f423b5559ff74016c
ryanforsyth <ryan.forsyth@digital.justice.gov.uk>
2021-09-23 11:04:12 +0100

    substituded physical delivery session tables for the views

---
+++ src/main/resources/db/migration/V1_82__create_delivery_session_tables.sql
@@ -0,0 +1,25 @@
+BEGIN;
+
+    drop view delivery_session;
+    drop view delivery_session_appointment;
+
+    create table delivery_session as
+    select id, referral_id, session_number from action_plan_session
+    where latest_approved = true;
+
+    alter table delivery_session add constraint pk_delivery_session primary key (id);
+    alter table delivery_session add constraint fk_delivery_session_referral foreign key (referral_id) references referral;
+    alter table delivery_session add constraint bk_delivery_session unique (referral_id, session_number);
+
+    create table delivery_session_appointment as
+    select action_plan_session_id delivery_session_id, appointment_id from action_plan_session_appointment apsa
+    inner join delivery_session ds on ds.id = apsa.action_plan_session_id;
+
+    alter table delivery_session_appointment
+        add constraint pk_delivery_session_appointment primary key (delivery_session_id, appointment_id);
+    alter table delivery_session_appointment
+        add constraint fk_delivery_session_appointment_delivery_session foreign key (delivery_session_id) references delivery_session;
+    alter table delivery_session_appointment
+        add constraint fk_delivery_session_appointment_appointment foreign key (appointment_id) references appointment;
+
+COMMIT;
