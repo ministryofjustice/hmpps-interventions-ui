12073be84f165fd4c5c542da2421c9f5f6a10924
ryanforsyth <ryan.forsyth@digital.justice.gov.uk>
2021-09-29 11:54:23 +0100

    add comment explaining the migration

--- src/main/resources/db/migration/V1_80__latest_approved_action_plan.sql
+++ src/main/resources/db/migration/V1_80__latest_approved_action_plan.sql
@@ -1,3 +1,12 @@
+/*
+    We do not need to worry about non-approved action plans as the session table will only contain sessions for approved action plans.
+    Add a new column (_latest_approved_) to every action plan session which denotes if this session belongs to the latest approved action plan
+    Select all approved action plans grouped by referral id and order them according to approval date. This allows us to easily select the most recently approved action plan for a given referral.
+    Update every action plan session setting _latest_approved_ to true for sessions associated with the most recently approved action plan.
+    Create views of action plan sessions with their id, referral id and session number so we can refer to these as _delivery_session_
+    Create views of action plan session appointments in a similar fashion.
+*/
+
 BEGIN;
 alter table action_plan_session add column latest_approved boolean not null default true;
 
