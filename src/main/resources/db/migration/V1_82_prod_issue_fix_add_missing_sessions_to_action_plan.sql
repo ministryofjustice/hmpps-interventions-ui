e8b4bb776e4b2d01021afd9d461dabe25721e9dd
Andrew Burrows <andrew.burrows@digital.justice.gov.uk>
2021-10-15 12:15:49 +0100

    User notes that they have missing sessions on their approved action plan.
    The action plan was approved with 8 sessions but they can only book 5 sessions.
    
    This is because there are two action plans tied to the referral, one with 5 and one with 8.
    
    This was caused by a bug that was introduced into production where we deployed action plan versioning too early such that action plans can turn into an invalid state.
    
    To fix the issue we simply just need to add the missing sessions on the action_plan_session table.

---
+++ src/main/resources/db/migration/V1_82_prod_issue_fix_add_missing_sessions_to_action_plan.sql
@@ -0,0 +1,3 @@
+insert into action_plan_session (id, session_number, deprecated_action_plan_id, referral_id, latest_approved) values (gen_random_uuid(), 6, 'b9cd89d0-6e6a-4856-8220-69fdb62732ea', 'c88ba538-aa56-442b-8aca-65582150a869', true);
+insert into action_plan_session (id, session_number, deprecated_action_plan_id, referral_id, latest_approved) values (gen_random_uuid(), 7, 'b9cd89d0-6e6a-4856-8220-69fdb62732ea', 'c88ba538-aa56-442b-8aca-65582150a869', true);
+insert into action_plan_session (id, session_number, deprecated_action_plan_id, referral_id, latest_approved) values (gen_random_uuid(), 8, 'b9cd89d0-6e6a-4856-8220-69fdb62732ea', 'c88ba538-aa56-442b-8aca-65582150a869', true);
