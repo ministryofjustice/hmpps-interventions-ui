ca0c26b9d54ff5ab38433c2eb1274dc12df3ed45
ryanforsyth <ryan.forsyth@digital.justice.gov.uk>
2021-10-15 12:24:06 +0100

    add index on delivery session and unique constraint on appointments table

--- src/main/resources/db/migration/V1_82__create_delivery_session_tables.sql
+++ src/main/resources/db/migration/V1_82__create_delivery_session_tables.sql
@@ -11,10 +11,14 @@ BEGIN;
     alter table delivery_session add constraint fk_delivery_session_referral foreign key (referral_id) references referral;
     alter table delivery_session add constraint bk_delivery_session unique (referral_id, session_number);
 
+    create index idx_referral__id on delivery_session (referral_id);
+
     create table delivery_session_appointment as
     select action_plan_session_id delivery_session_id, appointment_id from action_plan_session_appointment apsa
     inner join delivery_session ds on ds.id = apsa.action_plan_session_id;
 
+    alter table delivery_session_appointment
+        add constraint uk_delivery_session_appointments_appointments_id unique (appointments_id),
     alter table delivery_session_appointment
         add constraint pk_delivery_session_appointment primary key (delivery_session_id, appointment_id);
     alter table delivery_session_appointment
