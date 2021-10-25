8fdb7ac0cc4df0aa1141f8b2b64dab4e92e5509f
Andrew Burrows <andrew.burrows@digital.justice.gov.uk>
2021-10-25 13:27:19 +0100

    Removed unused method in ReferralAccessFilter. This service provider referral access check method was overriden by one that uses JPA Specification

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/authorization/ReferralAccessFilter.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/authorization/ReferralAccessFilter.kt
@@ -12,13 +12,6 @@ import java.util.UUID
 class ReferralAccessFilter(
   private val serviceProviderAccessScopeMapper: ServiceProviderAccessScopeMapper,
 ) {
-  fun serviceProviderReferrals(referrals: List<Referral>, user: AuthUser): List<Referral> {
-    val userScope = serviceProviderAccessScopeMapper.fromUser(user)
-
-    // fixme: how do equality operators work for these types of comparisons - should we write a custom equals override
-    //        that compares contract reference numbers?
-    return referrals.filter { userScope.contracts.contains(it.intervention.dynamicFrameworkContract) }
-  }
 
   fun serviceProviderReferrals(referralSpec: Specification<Referral>, user: AuthUser): Specification<Referral> {
     val userScope = serviceProviderAccessScopeMapper.fromUser(user)
