08c6c13585307bf710af59fa58b792a817e87e40
Andrew Burrows <andrew.burrows@digital.justice.gov.uk>
2021-11-08 11:14:56 +0000

    Added revert functionality for oasys risk information.

--- server/routes/makeAReferral/risk-information/oasys/edit/editOasysRiskInformationView.ts
+++ server/routes/makeAReferral/risk-information/oasys/edit/editOasysRiskInformationView.ts
@@ -17,98 +17,114 @@ export default class EditOasysRiskInformationView {
   private readonly errorSummaryArgs = ViewUtils.govukErrorSummaryArgs(this.presenter.errors.summary)
 
   private get whoIsAtRiskTextareaArgs(): TextareaArgs {
-    const whoIsAtRisk = this.draftOasysRiskInformation
-      ? this.draftOasysRiskInformation.riskSummaryWhoIsAtRisk
-      : this.riskSummaryView.riskInformation.summary.whoIsAtRisk.text
+    const oasysRiskInfoText = this.riskSummaryView.riskInformation.summary.whoIsAtRisk.text || ''
+    const whoIsAtRisk = this.draftOasysRiskInformation?.riskSummaryWhoIsAtRisk || oasysRiskInfoText
     return {
       name: 'who-is-at-risk',
       id: 'who-is-at-risk',
       label: {},
       value: ViewUtils.escape(whoIsAtRisk || ''),
+      attributes: {
+        oasysRiskInfo: ViewUtils.escape(oasysRiskInfoText),
+      },
     }
   }
 
   private get natureOfRiskTextareaArgs(): TextareaArgs {
-    const natureOfRisk = this.draftOasysRiskInformation
-      ? this.draftOasysRiskInformation.riskSummaryNatureOfRisk
-      : this.riskSummaryView.riskInformation.summary.natureOfRisk.text
+    const oasysRiskInfoText = this.riskSummaryView.riskInformation.summary.natureOfRisk.text || ''
+    const natureOfRisk = this.draftOasysRiskInformation?.riskSummaryNatureOfRisk || oasysRiskInfoText
     return {
       name: 'nature-of-risk',
       id: 'nature-of-risk',
       label: {},
       value: ViewUtils.escape(natureOfRisk || ''),
+      attributes: {
+        oasysRiskInfo: ViewUtils.escape(oasysRiskInfoText),
+      },
     }
   }
 
   private get riskImminenceTextareaArgs(): TextareaArgs {
-    const riskImminence = this.draftOasysRiskInformation
-      ? this.draftOasysRiskInformation.riskSummaryRiskImminence
-      : this.riskSummaryView.riskInformation.summary.riskImminence.text
+    const oasysRiskInfoText = this.riskSummaryView.riskInformation.summary.riskImminence.text || ''
+    const riskImminence = this.draftOasysRiskInformation?.riskSummaryRiskImminence || oasysRiskInfoText
     return {
       name: 'risk-imminence',
       id: 'risk-imminence',
       label: {},
       value: ViewUtils.escape(riskImminence || ''),
+      attributes: {
+        oasysRiskInfo: ViewUtils.escape(oasysRiskInfoText),
+      },
     }
   }
 
   private get riskToSelfSelfHarmTextareaArgs(): TextareaArgs {
-    const selfHarm = this.draftOasysRiskInformation
-      ? this.draftOasysRiskInformation.riskToSelfSelfHarm
-      : this.riskSummaryView.riskInformation.riskToSelf.selfHarm.text
+    const oasysRiskInfoText = this.riskSummaryView.riskInformation.riskToSelf.selfHarm.text || ''
+    const selfHarm = this.draftOasysRiskInformation?.riskToSelfSelfHarm || oasysRiskInfoText
     return {
       name: 'risk-to-self-self-harm',
       id: 'risk-to-self-self-harm',
       label: {},
       value: ViewUtils.escape(selfHarm || ''),
+      attributes: {
+        oasysRiskInfo: ViewUtils.escape(oasysRiskInfoText),
+      },
     }
   }
 
   private get riskToSelfSuicideTextareaArgs(): TextareaArgs {
-    const suicide = this.draftOasysRiskInformation
-      ? this.draftOasysRiskInformation.riskToSelfSuicide
-      : this.riskSummaryView.riskInformation.riskToSelf.suicide.text
+    const oasysRiskInfoText = this.riskSummaryView.riskInformation.riskToSelf.suicide.text || ''
+    const suicide = this.draftOasysRiskInformation?.riskToSelfSuicide || oasysRiskInfoText
     return {
       name: 'risk-to-self-suicide',
       id: 'risk-to-self-suicide',
       label: {},
       value: ViewUtils.escape(suicide || ''),
+      attributes: {
+        oasysRiskInfo: ViewUtils.escape(oasysRiskInfoText),
+      },
     }
   }
 
   private get riskToSelfHostelSettingTextareaArgs(): TextareaArgs {
-    const hostelSetting = this.draftOasysRiskInformation
-      ? this.draftOasysRiskInformation.riskToSelfHostelSetting
-      : this.riskSummaryView.riskInformation.riskToSelf.hostelSetting.text
+    const oasysRiskInfoText = this.riskSummaryView.riskInformation.riskToSelf.hostelSetting.text || ''
+    const hostelSetting = this.draftOasysRiskInformation?.riskToSelfHostelSetting || oasysRiskInfoText
     return {
       name: 'risk-to-self-hostel-setting',
       id: 'risk-to-self-hostel-setting',
       label: {},
       value: ViewUtils.escape(hostelSetting || ''),
+      attributes: {
+        oasysRiskInfo: ViewUtils.escape(oasysRiskInfoText),
+      },
     }
   }
 
   private get riskToSelfVulnerabilityTextareaArgs(): TextareaArgs {
-    const vulnerability = this.draftOasysRiskInformation
-      ? this.draftOasysRiskInformation.riskToSelfVulnerability
-      : this.riskSummaryView.riskInformation.riskToSelf.vulnerability.text
+    const oasysRiskInfoText = this.riskSummaryView.riskInformation.riskToSelf.vulnerability.text || ''
+    const vulnerability = this.draftOasysRiskInformation?.riskToSelfVulnerability || oasysRiskInfoText
     return {
       name: 'risk-to-self-vulnerability',
       id: 'risk-to-self-vulnerability',
       label: {},
       value: ViewUtils.escape(vulnerability || ''),
+      attributes: {
+        oasysRiskInfo: ViewUtils.escape(oasysRiskInfoText),
+      },
     }
   }
 
   private get additionalInformationTextareaArgs(): TextareaArgs {
-    const additionalInformation = this.draftOasysRiskInformation
-      ? this.draftOasysRiskInformation.additionalInformation
-      : this.riskSummaryView.riskInformation.additionalRiskInformation.text
+    const oasysRiskInfoText = this.riskSummaryView.riskInformation.additionalRiskInformation.text || ''
+    const additionalInformation = this.draftOasysRiskInformation?.additionalInformation || oasysRiskInfoText
     return {
       name: 'additional-information',
       id: 'additional-information',
       label: {},
       value: ViewUtils.escape(additionalInformation || ''),
+      attributes: {
+        oasysRiskInfo: ViewUtils.escape(oasysRiskInfoText),
+      },
     }
   }
 
