7034e621f900dd016b4fbfa14ba2640383f010f1
ryanforsyth <ryan.forsyth@digital.justice.gov.uk>
2021-11-11 11:12:03 +0000

    enable writing to csv for reporting job

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/reporting/BatchUtils.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/reporting/BatchUtils.kt
@@ -7,6 +7,9 @@ import org.springframework.batch.item.ItemWriter
 import org.springframework.batch.item.file.FlatFileHeaderCallback
 import org.springframework.batch.item.file.FlatFileItemWriter
 import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder
+import org.springframework.batch.item.file.transform.BeanWrapperFieldExtractor
+import org.springframework.batch.item.file.transform.DelimitedLineAggregator
+import org.springframework.batch.item.file.transform.RecursiveCollectionLineAggregator
 import org.springframework.core.io.Resource
 import org.springframework.stereotype.Component
 import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.jpa.entity.Referral
@@ -31,34 +34,58 @@ class BatchUtils {
     return date.toInstant().atOffset(zoneOffset)
   }
 
+  private fun <T> csvFileWriterBase(
+    name: String,
+    resource: Resource,
+    headers: List<String>,
+  ): FlatFileItemWriterBuilder<T> {
+    return FlatFileItemWriterBuilder<T>()
+      .name(name)
+      .resource(resource)
+      .headerCallback(HeaderWriter(headers.joinToString(",")))
+  }
+
+  private fun <T> csvLineAggregator(fields: List<String>): DelimitedLineAggregator<T> {
+    return DelimitedLineAggregator<T>().apply {
+      setDelimiter(",")
+      setFieldExtractor(
+        BeanWrapperFieldExtractor<T>().apply {
+          setNames(fields.toTypedArray())
+          afterPropertiesSet()
+        }
+      )
+    }
+  }
+
   fun <T> csvFileWriter(
     name: String,
     resource: Resource,
     headers: List<String>,
     fields: List<String>,
   ): FlatFileItemWriter<T> {
-    return FlatFileItemWriterBuilder<T>()
-      .name(name)
-      .resource(resource)
-      .headerCallback(HeaderWriter(headers.joinToString(",")))
-      .delimited()
-      .delimiter(",")
-      .names(*fields.toTypedArray())
+    return csvFileWriterBase<T>(name, resource, headers)
+      .lineAggregator(csvLineAggregator(fields))
       .build()
+  }
+
+  fun <T> recursiveCollectionCsvFileWriter(
+    name: String,
+    resource: Resource,
+    headers: List<String>,
+    fields: List<String>,
+  ): FlatFileItemWriter<Collection<T>> {
+    return csvFileWriterBase<Collection<T>>(name, resource, headers)
+      .lineAggregator(
+        RecursiveCollectionLineAggregator<T>().apply {
+          setDelegate(csvLineAggregator(fields))
+        }
+      ).build()
   }
 }
 
 class HeaderWriter(private val header: String) : FlatFileHeaderCallback {
   override fun writeHeader(writer: Writer) {
     writer.write(header)
-  }
-}
-
-class LoggingWriter<T> : ItemWriter<T> {
-  companion object : KLogging()
-
-  override fun write(items: MutableList<out T>) {
-    logger.info(items.toString())
   }
 }
 
@@ -71,5 +98,13 @@ interface SentReferralProcessor<T> : ItemProcessor<Referral, T> {
     logger.debug("processing referral {}", StructuredArguments.kv("referralId", referral.id))
     if (referral.sentAt == null) throw RuntimeException("invalid referral passed to sent referral processor; referral has not been sent")
     return processSentReferral(referral)
+  }
+}
+
+class LoggingWriter<T> : ItemWriter<T> {
+  companion object : KLogging()
+
+  override fun write(items: MutableList<out T>) {
+    logger.info(items.toString())
   }
 }
