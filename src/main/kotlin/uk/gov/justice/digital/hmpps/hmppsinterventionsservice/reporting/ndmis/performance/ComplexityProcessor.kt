4481beba698eecedfbfc3f4715a0b1053c036901
ryanforsyth <ryan.forsyth@digital.justice.gov.uk>
2021-11-15 11:46:47 +0000

    skip rows using skip policy rather than try catch

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/reporting/ndmis/performance/ComplexityProcessor.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/reporting/ndmis/performance/ComplexityProcessor.kt
@@ -1,7 +1,6 @@
 package uk.gov.justice.digital.hmpps.hmppsinterventionsservice.reporting.ndmis.performance
 
 import mu.KLogging
-import net.logstash.logback.argument.StructuredArguments
 import org.springframework.stereotype.Component
 import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.jpa.entity.Referral
 import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.reporting.SentReferralProcessor
@@ -10,26 +9,18 @@ import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.reporting.SentRefe
 class ComplexityProcessor() : SentReferralProcessor<List<ComplexityData>> {
   companion object : KLogging()
 
-  override fun processSentReferral(referral: Referral): List<ComplexityData>? {
-    try {
-      return referral.selectedServiceCategories!!.map {
-        ComplexityData(
-          referralReference = referral.referenceNumber!!,
-          referralId = referral.id,
-          interventionTitle = referral.intervention.title,
-          serviceCategoryId = it.id,
-          serviceCategoryName = it.name,
-          complexityLevelTitle = it.complexityLevels.find { complexityLevel ->
-            complexityLevel.id == referral.complexityLevelIds!![it.id]
-          }!!.title
-        )
-      }
-    } catch (e: NullPointerException) {
-      logger.warn(
-        "Null pointer found when processing complexity report {}",
-        StructuredArguments.kv("referral", referral.id),
+  override fun processSentReferral(referral: Referral): List<ComplexityData> {
+    return referral.selectedServiceCategories!!.map {
+      ComplexityData(
+        referralReference = referral.referenceNumber!!,
+        referralId = referral.id,
+        interventionTitle = referral.intervention.title,
+        serviceCategoryId = it.id,
+        serviceCategoryName = it.name,
+        complexityLevelTitle = it.complexityLevels.find { complexityLevel ->
+          complexityLevel.id == referral.complexityLevelIds!![it.id]
+        }!!.title
       )
-      return null
     }
   }
 }
