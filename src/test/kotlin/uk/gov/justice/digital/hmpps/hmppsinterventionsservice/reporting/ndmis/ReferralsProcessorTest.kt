1e5010bc0cc250f7dfd142e169ad2a833eb19cdc
David Lantos <david.lantos@gmail.com>
2021-11-17 12:09:56 +0000

    Switch back to the old psql-like datetime format
    We reached a compromise with the NDMIS team that we would retain the
    previous datetime format that psql gave us:
    
    `2021-11-13 17:55:51.067867+00` in winter (GMT)
    `2021-06-11 16:06:38.662521+00` in summer (BST)
    
    The ISO format we use now is:
    
    `2021-11-13T17:55:51.067867Z` in winter (GMT)
    `2021-06-11T17:06:38.662521+01:00` in summer (BST)
    
    This is because changing the ETL pipeline requires re-prioritisation of
    other work, even for a datetime parsing change
    
    ---
    
    I opted for a separate data class, to make this super explicit

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/reporting/ndmis/ReferralsProcessorTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/reporting/ndmis/ReferralsProcessorTest.kt
@@ -6,10 +6,12 @@ import com.nhaarman.mockitokotlin2.whenever
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.reporting.ndmis.performance.NdmisDateTime
 import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.reporting.ndmis.performance.ReferralsProcessor
 import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.service.ActionPlanService
 import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.util.ActionPlanFactory
 import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.util.AppointmentFactory
+import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.util.EndOfServiceReportFactory
 import uk.gov.justice.digital.hmpps.hmppsinterventionsservice.util.ReferralFactory
 import java.time.OffsetDateTime
 
@@ -18,6 +20,7 @@ internal class ReferralsProcessorTest {
   private val processor = ReferralsProcessor(actionPlanService)
 
   private val referralFactory = ReferralFactory()
+  private val eosrFactory = EndOfServiceReportFactory()
   private val appointmentFactory = AppointmentFactory()
   private val actionPlanFactory = ActionPlanFactory()
 
@@ -30,9 +33,22 @@ internal class ReferralsProcessorTest {
 
   @Test
   fun `referral correctly returns a referralData object`() {
-    val actionPlanFirst = actionPlanFactory.createApproved(submittedAt = OffsetDateTime.now(), approvedAt = OffsetDateTime.now())
-    val actionPlanSecond = actionPlanFactory.createApproved(submittedAt = OffsetDateTime.now().plusDays(1), approvedAt = OffsetDateTime.now().plusDays(1))
-    val referral = referralFactory.createSent(actionPlans = mutableListOf(actionPlanFirst, actionPlanSecond))
+    val sentAt = OffsetDateTime.parse("2021-11-15T09:01:10.486887Z")
+    val apSubmittedAt = OffsetDateTime.parse("2021-11-15T15:01:10.486887Z")
+    val apApprovedAt = OffsetDateTime.parse("2021-11-15T17:11:10.486887Z")
+    val endRequestedAt = OffsetDateTime.parse("2021-11-16T14:42:20.123456Z")
+    val eosrSubmittedAt = OffsetDateTime.parse("2021-11-17T08:59:10.486887Z")
+    val concludedAt = OffsetDateTime.parse("2021-11-17T09:01:10.486887Z")
+
+    val actionPlanFirst = actionPlanFactory.createApproved(submittedAt = apSubmittedAt, approvedAt = apApprovedAt)
+    val actionPlanSecond = actionPlanFactory.createApproved(submittedAt = apSubmittedAt.plusDays(1), approvedAt = apApprovedAt.plusDays(1))
+    val referral = referralFactory.createEnded(
+      sentAt = sentAt,
+      concludedAt = concludedAt,
+      endRequestedAt = endRequestedAt,
+      endOfServiceReport = eosrFactory.create(submittedAt = eosrSubmittedAt),
+      actionPlans = mutableListOf(actionPlanFirst, actionPlanSecond)
+    )
 
     whenever(actionPlanService.getAllAttendedAppointments(anyOrNull())).thenReturn(listOf(appointmentFactory.create()))
 
@@ -43,20 +59,20 @@ internal class ReferralsProcessorTest {
     assertThat(result.contractReference).isEqualTo(referral.intervention.dynamicFrameworkContract.contractReference)
     assertThat(result.contractType).isEqualTo(referral.intervention.dynamicFrameworkContract.contractType.name)
     assertThat(result.primeProvider).isEqualTo(referral.intervention.dynamicFrameworkContract.primeProvider.name)
-    assertThat(result.relevantSentanceId).isEqualTo(1234567L)
+    assertThat(result.relevantSentanceId).isEqualTo(123456L)
     assertThat(result.serviceUserCRN).isEqualTo("X123456")
-    assertThat(result.dateReferralReceived).isEqualTo(referral.sentAt)
-    assertThat(result.firstActionPlanSubmittedAt).isEqualTo(actionPlanFirst.submittedAt)
-    assertThat(result.firstActionPlanApprovedAt).isEqualTo(actionPlanFirst.approvedAt)
+    assertThat(result.dateReferralReceived).isEqualTo(NdmisDateTime(sentAt))
+    assertThat(result.firstActionPlanSubmittedAt).isEqualTo(NdmisDateTime(apSubmittedAt))
+    assertThat(result.firstActionPlanApprovedAt).isEqualTo(NdmisDateTime(apApprovedAt))
     assertThat(result.numberOfOutcomes).isEqualTo(referral.selectedDesiredOutcomes?.size)
     assertThat(result.achievementScore).isEqualTo(referral.endOfServiceReport?.achievementScore)
     assertThat(result.numberOfSessions).isEqualTo(referral.approvedActionPlan?.numberOfSessions)
     assertThat(result.numberOfSessionsAttended).isEqualTo(1)
-    assertThat(result.endRequestedAt).isEqualTo(referral.endRequestedAt)
+    assertThat(result.endRequestedAt).isEqualTo(NdmisDateTime(endRequestedAt))
     assertThat(result.endRequestedReason).isEqualTo(referral.endRequestedReason?.description)
-    assertThat(result.eosrSubmittedAt).isEqualTo(referral.endOfServiceReport?.submittedAt)
+    assertThat(result.eosrSubmittedAt).isEqualTo(NdmisDateTime(eosrSubmittedAt))
     assertThat(result.endReasonCode).isEqualTo(referral.endRequestedReason?.code)
     assertThat(result.endReasonDescription).isEqualTo(referral.endRequestedComments)
-    assertThat(result.concludedAt).isEqualTo(referral.concludedAt)
+    assertThat(result.concludedAt).isEqualTo(NdmisDateTime(concludedAt))
   }
 }
