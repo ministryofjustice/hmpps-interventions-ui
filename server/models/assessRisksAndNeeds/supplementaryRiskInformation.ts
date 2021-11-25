865e3b7993de5a742dc5e56be97b85ae4d5203cb
Andrew Burrows <andrew.burrows@digital.justice.gov.uk>
2021-11-25 14:43:34 +0000

    Added redactedRisk values into SupplementaryRiskInformation that is returned by ARN. This is the information that was entered in as part of MakeAReferral journey

--- server/models/assessRisksAndNeeds/supplementaryRiskInformation.ts
+++ server/models/assessRisksAndNeeds/supplementaryRiskInformation.ts
@@ -1,3 +1,12 @@
 export interface SupplementaryRiskInformation {
   riskSummaryComments: string
+  redactedRisk: {
+    riskWho: string
+    riskWhen: string
+    riskNature: string
+    concernsSelfHarm: string
+    concernsSuicide: string
+    concernsHostel: string
+    concernsVulnerability: string
+  }
 }
