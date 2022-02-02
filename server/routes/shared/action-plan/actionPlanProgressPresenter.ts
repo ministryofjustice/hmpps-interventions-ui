da402135b31c7c87bab6bb8e70e343b06be213fa
Tom Myers <tom.myers@digital.justice.gov.uk>
2022-02-02 16:50:14 +0000

    add new presenter and view classes for showing all action plans for a referral

---
+++ server/routes/shared/action-plan/actionPlanProgressPresenter.ts
@@ -0,0 +1,38 @@
+import ActionPlan from '../../../models/actionPlan'
+import ApprovedActionPlanSummary from '../../../models/approvedActionPlanSummary'
+import ActionPlanSummaryPresenter from './actionPlanSummaryPresenter'
+
+export default class ActionPlanProgressPresenter {
+  currentActionPlanSummaryPresenter: ActionPlanSummaryPresenter
+
+  constructor(
+    private readonly referralId: string,
+    private readonly currentActionPlan: ActionPlan | null,
+    readonly approvedActionPlanSummaries: ApprovedActionPlanSummary[],
+    readonly userType: 'service-provider' | 'probation-practitioner'
+  ) {
+    this.currentActionPlanSummaryPresenter = new ActionPlanSummaryPresenter(currentActionPlan, userType)
+  }
+
+  readonly tableHeaders = ['Version', 'Status', 'Submitted', 'Approved', 'Action']
+
+  readonly viewCurrentActionPlanUrl = `/${this.userType}/referrals/${this.referralId}/action-plan`
+
+  readonly createNewActionPlanUrl = `/service-provider/referrals/${this.referralId}/action-plan`
+
+  readonly continueInProgressActionPlanUrl = `/service-provider/action-plan/${this.currentActionPlan?.id}/add-activity/1`
+
+  get includeCurrentActionPlanRow(): boolean {
+    // if the current action plan is approved, it will be rendered from `approvedActionPlanSummaries`
+    if (this.currentActionPlanSummaryPresenter.actionPlanApproved) {
+      return false
+    }
+
+    // PPs should never see unsubmitted action plans
+    if (this.userType === 'probation-practitioner' && !this.currentActionPlanSummaryPresenter.actionPlanSubmitted) {
+      return false
+    }
+
+    return true
+  }
+}
