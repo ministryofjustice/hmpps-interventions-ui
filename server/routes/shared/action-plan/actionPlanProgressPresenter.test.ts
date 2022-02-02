8165f0e6e623f3e3df84773fd044bbb48d6fa83b
Tom Myers <tom.myers@digital.justice.gov.uk>
2022-02-02 16:50:14 +0000

    add new presenter and view classes for showing all action plans for a referral

---
+++ server/routes/shared/action-plan/actionPlanProgressPresenter.test.ts
@@ -0,0 +1,71 @@
+import actionPlanFactory from '../../../../testutils/factories/actionPlan'
+import referralFactory from '../../../../testutils/factories/sentReferral'
+import ActionPlanProgressPresenter from './actionPlanProgressPresenter'
+
+describe(ActionPlanProgressPresenter, () => {
+  const referral = referralFactory.build()
+  describe('createNewActionPlanUrl', () => {
+    it('returns the relative URL for creating a draft action plan', () => {
+      const presenter = new ActionPlanProgressPresenter(referral.id, null, [], 'service-provider')
+
+      expect(presenter.createNewActionPlanUrl).toEqual(`/service-provider/referrals/${referral.id}/action-plan`)
+    })
+  })
+
+  describe('continueInProgressActionPlanUrl', () => {
+    it('returns the relative URL for continuing a draft action plan', () => {
+      const actionPlan = actionPlanFactory.justCreated(referral.id).build()
+      const presenter = new ActionPlanProgressPresenter(referral.id, actionPlan, [], 'probation-practitioner')
+
+      expect(presenter.continueInProgressActionPlanUrl).toEqual(
+        `/service-provider/action-plan/${actionPlan.id}/add-activity/1`
+      )
+    })
+  })
+
+  describe('viewCurrentActionPlanUrl', () => {
+    it('returns the relative URL for viewing the current action plan', () => {
+      const actionPlan = actionPlanFactory.submitted().build({ referralId: referral.id })
+      const presenter = new ActionPlanProgressPresenter(referral.id, actionPlan, [], 'probation-practitioner')
+
+      expect(presenter.viewCurrentActionPlanUrl).toEqual(`/probation-practitioner/referrals/${referral.id}/action-plan`)
+    })
+  })
+
+  describe('includeCurrentActionPlanRow', () => {
+    it('returns false if the current action plan is approved', () => {
+      const actionPlan = actionPlanFactory.approved().build({ referralId: referral.id })
+      const spPresenter = new ActionPlanProgressPresenter(referral.id, actionPlan, [], 'service-provider')
+      const ppPresenter = new ActionPlanProgressPresenter(referral.id, actionPlan, [], 'probation-practitioner')
+
+      expect(spPresenter.includeCurrentActionPlanRow).toEqual(false)
+      expect(ppPresenter.includeCurrentActionPlanRow).toEqual(false)
+    })
+
+    it('returns false for PPs if the referral has not been created', () => {
+      const spPresenter = new ActionPlanProgressPresenter(referral.id, null, [], 'service-provider')
+      const ppPresemnter = new ActionPlanProgressPresenter(referral.id, null, [], 'probation-practitioner')
+
+      expect(spPresenter.includeCurrentActionPlanRow).toEqual(true)
+      expect(ppPresemnter.includeCurrentActionPlanRow).toEqual(false)
+    })
+
+    it('returns false for PPs if the referral is not submitted', () => {
+      const actionPlan = actionPlanFactory.justCreated(referral.id).build()
+      const spPresenter = new ActionPlanProgressPresenter(referral.id, actionPlan, [], 'service-provider')
+      const ppPresemnter = new ActionPlanProgressPresenter(referral.id, actionPlan, [], 'probation-practitioner')
+
+      expect(spPresenter.includeCurrentActionPlanRow).toEqual(true)
+      expect(ppPresemnter.includeCurrentActionPlanRow).toEqual(false)
+    })
+
+    it('returns true for submitted action plans', () => {
+      const actionPlan = actionPlanFactory.submitted().build({ referralId: referral.id })
+      const spPresenter = new ActionPlanProgressPresenter(referral.id, actionPlan, [], 'service-provider')
+      const ppPresemnter = new ActionPlanProgressPresenter(referral.id, actionPlan, [], 'probation-practitioner')
+
+      expect(spPresenter.includeCurrentActionPlanRow).toEqual(true)
+      expect(ppPresemnter.includeCurrentActionPlanRow).toEqual(true)
+    })
+  })
+})
--- server/utils/actionPlanUtils.ts
+++ server/utils/actionPlanUtils.ts
@@ -1,4 +1,12 @@
 import ApprovedActionPlanSummary from '../models/approvedActionPlanSummary'
+import { TagArgs } from './govukFrontendTypes'
+
+export enum ActionPlanStatus {
+  Approved = 'Approved',
+  AwaitingApproval = 'Awaiting approval',
+  InDraft = 'In draft',
+  NotSubmitted = 'Not submitted',
+}
 
 export default class ActionPlanUtils {
   static getLatestApprovedActionPlanSummary(
@@ -9,5 +17,26 @@ export default class ActionPlanUtils {
         return new Date(a.approvedAt) >= new Date(b.approvedAt) ? -1 : 1
       })?.[0] ?? null
     )
+  }
+
+  private static actionPlanTagClass(actionPlanStatus: ActionPlanStatus): string {
+    switch (actionPlanStatus) {
+      case ActionPlanStatus.Approved:
+        return 'govuk-tag--green'
+      case ActionPlanStatus.AwaitingApproval:
+        return 'govuk-tag--red'
+      case ActionPlanStatus.InDraft:
+        return 'govuk-tag--yellow'
+      default:
+        return 'govuk-tag--grey'
+    }
+  }
+
+  static actionPlanTagArgs(status: ActionPlanStatus): TagArgs {
+    return {
+      text: status,
+      classes: ActionPlanUtils.actionPlanTagClass(status),
+      attributes: { id: 'action-plan-status' },
+    }
   }
 }
