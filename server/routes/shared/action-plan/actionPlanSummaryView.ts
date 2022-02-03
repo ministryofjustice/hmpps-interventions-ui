0c2711da977f42ac3c72e98509fa96777eb371a2
Tom Myers <tom.myers@digital.justice.gov.uk>
2022-02-03 23:51:59 +0000

    remove 'todo' actions and associated properties from 'actionPlanSummary' classes

--- server/routes/shared/action-plan/actionPlanSummaryView.ts
+++ server/routes/shared/action-plan/actionPlanSummaryView.ts
@@ -1,33 +1,16 @@
 import { SummaryListArgs, SummaryListArgsRow, TagArgs } from '../../../utils/govukFrontendTypes'
-import ViewUtils from '../../../utils/viewUtils'
 import ActionPlanSummaryPresenter from './actionPlanSummaryPresenter'
+import ActionPlanUtils from '../../../utils/actionPlanUtils'
 
 export default class ActionPlanSummaryView {
-  constructor(private readonly presenter: ActionPlanSummaryPresenter, private readonly includeTodo: boolean) {}
+  constructor(private readonly presenter: ActionPlanSummaryPresenter) {}
 
-  private get actionPlanTagClass(): string {
-    switch (this.presenter.text.actionPlanStatus) {
-      case 'Approved':
-        return 'govuk-tag--green'
-      case 'Awaiting approval':
-        return 'govuk-tag--red'
-      case 'In draft':
-        return 'govuk-tag--yellow'
-      default:
-        return 'govuk-tag--grey'
-    }
-  }
-
-  summaryListArgs(tagMacro: (args: TagArgs) => string, csrfToken?: string): SummaryListArgs {
+  summaryListArgs(tagMacro: (args: TagArgs) => string): SummaryListArgs {
     const rows: SummaryListArgsRow[] = [
       {
         key: { text: 'Action plan status' },
         value: {
-          text: tagMacro({
-            text: this.presenter.text.actionPlanStatus,
-            classes: this.actionPlanTagClass,
-            attributes: { id: 'action-plan-status' },
-          }),
+          text: tagMacro(ActionPlanUtils.actionPlanTagArgs(this.presenter.actionPlanStatus)),
         },
       },
     ]
@@ -47,38 +30,6 @@ export default class ActionPlanSummaryView {
         key: { text: 'Approval date' },
         value: { text: this.presenter.text.actionPlanApprovalDate },
       })
-    }
-
-    if (this.includeTodo) {
-      if (this.presenter.actionPlanSubmitted) {
-        rows.push({
-          key: { text: 'Action' },
-          value: {
-            html: `<a href="${this.presenter.viewActionPlanUrl}" class="govuk-link">View action plan</a>`,
-          },
-        })
-      } else if (this.presenter.userType === 'service-provider') {
-        if (!this.presenter.actionPlanCreated) {
-          rows.push({
-            key: { text: 'Action' },
-            value: {
-              html: `<form method="post" action="${ViewUtils.escape(this.presenter.createActionPlanFormAction)}">
-                     <input type="hidden" name="_csrf" value="${ViewUtils.escape(csrfToken!)}">
-                     <button data-module="govuk-button" data-prevent-double-click="true">
-                       Create action plan
-                     </button>
-                     </form>`,
-            },
-          })
-        } else if (!this.presenter.actionPlanSubmitted) {
-          rows.push({
-            key: { text: 'Action' },
-            value: {
-              html: `<a href="${this.presenter.actionPlanFormUrl}" class="govuk-link">Complete action plan</a>`,
-            },
-          })
-        }
-      }
     }
 
     return { rows }
