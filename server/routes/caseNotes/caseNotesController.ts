4c167ea81c6f3eefca0b9cf599af6c71d1d6abae
ryanforsyth <ryan.forsyth@digital.justice.gov.uk>
2022-02-03 11:28:15 +0000

    pass query param to specify the page the case note was located on to redirect back to via backlink

--- server/routes/caseNotes/caseNotesController.ts
+++ server/routes/caseNotes/caseNotesController.ts
@@ -134,13 +134,20 @@ export default class CaseNotesController {
   ): Promise<void> {
     const { accessToken } = res.locals.user.token
     const { caseNoteId } = req.params
+    const backlinkPageNumber = req.query.backlinkPageNumber as string
+    if (!Number(backlinkPageNumber)) {
+      // This is to prevent the url being manipulated.
+      throw createError(500, `The page number for a case not backlink should only ever be a number.`, {
+        userMessage: 'Incorrect url.',
+      })
+    }
     const caseNote = await this.interventionsService.getCaseNote(accessToken, caseNoteId)
     const sentByUserDetails = await this.hmppsAuthService.getUserDetailsByUsername(
       accessToken,
       caseNote.sentBy.username
     )
     const sentByUserName = sentByUserDetails.name
-    const presenter = new CaseNotePresenter(caseNote, sentByUserName, loggedInUserType)
+    const presenter = new CaseNotePresenter(caseNote, sentByUserName, loggedInUserType, backlinkPageNumber)
     const view = new CaseNoteView(presenter)
     ControllerUtils.renderWithLayout(res, view, null)
   }
