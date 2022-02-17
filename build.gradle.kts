dc93be7c178bd87b5f7788c167dc30b2afa34f00
David Lantos <david.lantos@gmail.com>
2022-02-17 10:33:05 +0000

    Downgrade postgresql to 42.2.25
    The PostgreSQL upgrade to 42.3.x happened at the same time as we are
    rolling out dashboard pagination changes. We want to reduce the amount
    of changes so we can reliably monitor SQL performance.

--- build.gradle.kts
+++ build.gradle.kts
@@ -61,16 +61,13 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
   implementation("com.nimbusds:oauth2-oidc-sdk:9.25")
 
-  // bumps for security, until bumped in upstream
-  implementation("org.postgresql:postgresql:42.3.2") // CVE-2022-21724
-
   // database
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("org.springframework.boot:spring-boot-starter-validation")
   implementation("org.hibernate:hibernate-core:5.6.5.Final")
   implementation("com.vladmihalcea:hibernate-types-52:2.14.0")
   runtimeOnly("org.flywaydb:flyway-core")
-  runtimeOnly("org.postgresql:postgresql")
+  runtimeOnly("org.postgresql:postgresql:42.2.25")
 
   // json and csv
   implementation("com.github.java-json-tools:json-patch:1.13")
