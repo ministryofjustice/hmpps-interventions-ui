df55ec76ca3a46c7ba60f580a96d57572d564009
Tom Myers <tom.myers@digital.justice.gov.uk>
2022-03-09 15:16:01 +0000

    update integration tests to test sort headings and not actual sorting

--- integration_tests/integration/dashboards.spec.js
+++ integration_tests/integration/dashboards.spec.js
@@ -307,279 +307,69 @@ describe('Dashboards', () => {
       })
     })
 
-    describe('Sorting the table', () => {
-      describe('with "Date received" as second order sort', () => {
-        it('should order by "Date received" when first order values are identical', () => {
-          const sentReferralsWithIdenticalReferenceNumber = [
-            sentReferralFactory.assigned().build({
-              sentAt: '2021-01-27T13:00:00.000000Z',
-              referenceNumber: 'A',
-              referral: {
-                interventionId: accommodationIntervention.id,
-              },
-            }),
-            sentReferralFactory.assigned().build({
-              sentAt: '2021-01-26T13:00:00.000000Z',
-              referenceNumber: 'A',
-              referral: {
-                interventionId: accommodationIntervention.id,
-              },
-            }),
-            sentReferralFactory.assigned().build({
-              sentAt: '2021-01-28T13:00:00.000000Z',
-              referenceNumber: 'A',
-              referral: {
-                interventionId: accommodationIntervention.id,
-              },
-            }),
-            sentReferralFactory.assigned().build({
-              sentAt: '2021-01-26T13:00:00.000000Z',
-              referenceNumber: 'B',
-              referral: {
-                interventionId: accommodationIntervention.id,
-              },
-            }),
-          ]
+    describe('table sort headings', () => {
+      it('should show which column the table is currently sorted by', () => {
+        cy.stubGetSentReferralsForUserTokenPaged(pageFactory.pageContent([]).build())
+        cy.login()
 
-          cy.stubGetSentReferralsForUserTokenPaged(
-            pageFactory.pageContent(sentReferralsWithIdenticalReferenceNumber).build()
+        const headings = ['Date received', 'Referral', 'Service user', 'Intervention type']
+        headings.forEach(heading => {
+          cy.get('table').within(() => cy.contains('button', heading).click())
+
+          // check the clicked heading is sorted and all others are not
+          cy.get('thead')
+            .find('th')
+            .each($el => {
+              const sort = $el.text() === heading ? 'ascending' : 'none'
+              cy.wrap($el).should('have.attr', { 'aria-sort': sort })
+            })
+
+          // clicking again sorts in the other direction
+          cy.get('table').within(() => cy.contains('button', heading).click())
+          cy.get('table').within(() =>
+            cy.contains('button', heading).should('have.attr', { 'aria-sort': 'descending' })
           )
-          cy.login()
-
-          cy.get('table')
-            .getTable({ onlyColumns: ['Date received', 'Referral'] })
-            .should('deep.equal', [
-              {
-                'Date received': '27 Jan 2021',
-                Referral: 'A',
-              },
-              {
-                'Date received': '26 Jan 2021',
-                Referral: 'A',
-              },
-              {
-                'Date received': '28 Jan 2021',
-                Referral: 'A',
-              },
-              {
-                'Date received': '26 Jan 2021',
-                Referral: 'B',
-              },
-            ])
-
-          cy.get('table').within(() => cy.contains('button', 'Referral').click())
-          cy.get('table')
-            .getTable({ onlyColumns: ['Date received', 'Referral'] })
-            .should('deep.equal', [
-              {
-                'Date received': '26 Jan 2021',
-                Referral: 'A',
-              },
-              {
-                'Date received': '27 Jan 2021',
-                Referral: 'A',
-              },
-              {
-                'Date received': '28 Jan 2021',
-                Referral: 'A',
-              },
-              {
-                'Date received': '26 Jan 2021',
-                Referral: 'B',
-              },
-            ])
-
-          // Even when descending the first order column it should keep the Date received column ordered as ascending
-          cy.get('table').within(() => cy.contains('button', 'Referral').click())
-          cy.get('table')
-            .getTable({ onlyColumns: ['Date received', 'Referral'] })
-            .should('deep.equal', [
-              {
-                'Date received': '26 Jan 2021',
-                Referral: 'B',
-              },
-              {
-                'Date received': '26 Jan 2021',
-                Referral: 'A',
-              },
-              {
-                'Date received': '27 Jan 2021',
-                Referral: 'A',
-              },
-              {
-                'Date received': '28 Jan 2021',
-                Referral: 'A',
-              },
-            ])
         })
       })
-
-      const assignedToSelfA = sentReferralFactory.assigned().build({
-        sentAt: '2020-12-13T13:00:00.000000Z',
-        referenceNumber: 'A',
-        referral: {
-          interventionId: accommodationIntervention.id,
-          serviceUser: { firstName: 'Jenny', lastName: 'Jones' },
-        },
-      })
-
-      const assignedToSelfB = sentReferralFactory.assigned().build({
-        sentAt: '2021-01-26T13:00:00.000000Z',
-        referenceNumber: 'B',
-        referral: {
-          interventionId: womensServicesIntervention.id,
-          serviceUser: { firstName: 'George', lastName: 'Michael' },
-        },
-      })
-
-      const unassignedA = sentReferralFactory.unassigned().build({
-        sentAt: '2020-12-13T13:00:00.000000Z',
-        referenceNumber: 'A',
-        referral: {
-          interventionId: accommodationIntervention.id,
-          serviceUser: { firstName: 'Jenny', lastName: 'Jones' },
-        },
-      })
-
-      const unassignedB = sentReferralFactory.unassigned().build({
-        sentAt: '2021-01-26T13:00:00.000000Z',
-        referenceNumber: 'B',
-        referral: {
-          interventionId: womensServicesIntervention.id,
-          serviceUser: { firstName: 'George', lastName: 'Michael' },
-        },
-      })
-
-      const completedA = sentReferralFactory.concluded().build({
-        sentAt: '2020-12-13T13:00:00.000000Z',
-        referenceNumber: 'A',
-        referral: {
-          interventionId: accommodationIntervention.id,
-          serviceUser: { firstName: 'Jenny', lastName: 'Jones' },
-        },
-      })
-
-      const completedB = sentReferralFactory.concluded().build({
-        sentAt: '2021-01-26T13:00:00.000000Z',
-        referenceNumber: 'B',
-        referral: {
-          interventionId: womensServicesIntervention.id,
-          serviceUser: { firstName: 'George', lastName: 'Michael' },
-        },
-      })
-
-      const rowForReferralAWithNoCaseworkerColumn = {
-        'Date received': '13 Dec 2020',
-        Referral: 'A',
-        'Service user': 'Jenny Jones',
-        'Intervention type': 'Accommodation Services - West Midlands',
-        Action: 'View',
-      }
-      const rowForReferralBWithNoCaseworkerColumn = {
-        'Date received': '26 Jan 2021',
-        Referral: 'B',
-        'Service user': 'George Michael',
-        'Intervention type': "Women's Services - West Midlands",
-        Action: 'View',
-      }
-
-      const rowForReferralAWithCaseworker = {
-        'Date received': '13 Dec 2020',
-        Referral: 'A',
-        'Service user': 'Jenny Jones',
-        'Intervention type': 'Accommodation Services - West Midlands',
-        Caseworker: 'UserABC',
-        Action: 'View',
-      }
-
-      const rowForReferralBWithCaseworker = {
-        'Date received': '26 Jan 2021',
-        Referral: 'B',
-        'Service user': 'George Michael',
-        'Intervention type': "Women's Services - West Midlands",
-        Caseworker: 'UserABC',
-        Action: 'View',
-      }
 
       const dashBoardTables = [
         {
           dashboardType: 'My cases',
-          referrals: [assignedToSelfA, assignedToSelfB],
-          initialTable: [rowForReferralAWithNoCaseworkerColumn, rowForReferralBWithNoCaseworkerColumn],
-          sortedTable: [rowForReferralAWithNoCaseworkerColumn, rowForReferralBWithNoCaseworkerColumn],
-          sortedTables: [
-            {
-              header: 'Date received',
-            },
-          ],
+          sortField: 'Date received',
         },
         {
           dashboardType: 'All open cases',
-          referrals: [assignedToSelfA, assignedToSelfB],
-          initialTable: [rowForReferralAWithCaseworker, rowForReferralBWithCaseworker],
-          sortedTable: [rowForReferralAWithCaseworker, rowForReferralBWithCaseworker],
+          sortField: 'Intervention type',
         },
         {
           dashboardType: 'Unassigned cases',
-          referrals: [unassignedA, unassignedB],
-          initialTable: [rowForReferralAWithNoCaseworkerColumn, rowForReferralBWithNoCaseworkerColumn],
-          sortedTable: [rowForReferralAWithNoCaseworkerColumn, rowForReferralBWithNoCaseworkerColumn],
+          sortField: 'Service user',
         },
         {
           dashboardType: 'Completed cases',
-          referrals: [completedA, completedB],
-          initialTable: [rowForReferralAWithCaseworker, rowForReferralBWithCaseworker],
-          sortedTable: [rowForReferralAWithCaseworker, rowForReferralBWithCaseworker],
+          sortField: 'Referral',
         },
       ]
 
-      const referralToSelect = sentReferralFactory.build({
-        sentAt: '2021-01-26T13:00:00.000000Z',
-        referenceNumber: 'B',
-        referral: {
-          interventionId: accommodationIntervention.id,
-          serviceUser: { firstName: 'George', lastName: 'Michael' },
-        },
-      })
+      dashBoardTables.forEach(table => {
+        it(`persists the sort order when coming back to the page for dashboard "${table.dashboardType}"`, () => {
+          cy.stubGetSentReferralsForUserTokenPaged(pageFactory.pageContent([]).build())
+          cy.login()
 
-      dashBoardTables.forEach(({ dashboardType, referrals, initialTable, sortedTable }) => {
-        describe(`sorting by "Date received" for dashboard "${dashboardType}"`, () => {
-          beforeEach(() => {
-            cy.stubGetSentReferralsForUserTokenPaged(pageFactory.pageContent(referrals).build())
-          })
+          cy.contains(table.dashboardType).click()
 
-          it(`allows the user to sort by "Date received" for dashboard "${dashboardType}"`, () => {
-            cy.login()
-            cy.contains(dashboardType).click()
+          cy.get('table').within(() => cy.contains('button', table.sortField).click())
+          cy.get('table').within(() =>
+            cy.contains('button', table.sortField).should('have.attr', { 'aria-sort': 'ascending' })
+          )
 
-            cy.get('table').getTable().should('deep.equal', initialTable)
+          cy.contains(table.dashboardType).click()
 
-            cy.get('table').within(() => cy.contains('button', 'Date received').click())
-            cy.get('table').getTable().should('deep.equal', sortedTable)
-
-            cy.get('table').within(() => cy.contains('button', 'Date received').click())
-
-            const reversedTable = [...sortedTable].reverse()
-            cy.get('table').getTable().should('deep.equal', reversedTable)
-          })
-
-          it(`persists the sort order when coming back to the page for dashboard "${dashboardType}"`, () => {
-            cy.login()
-            cy.contains(dashboardType).click()
-
-            cy.get('table').within(() => cy.contains('button', 'Date received').click())
-            cy.get('table').getTable().should('deep.equal', sortedTable)
-
-            cy.stubViewReferralDetails(referralToSelect)
-
-            cy.visit(`/service-provider/referrals/${referralToSelect.id}/details`)
-            cy.contains('Back').click()
-            cy.contains(dashboardType).click()
-
-            // Wait for header sort button to load, as it means JS has run
-            cy.get('table').within(() => cy.contains('button', 'Date received'))
-            cy.get('table').getTable().should('deep.equal', sortedTable)
-          })
+          // Wait for header sort button to load, as it means JS has run
+          cy.get('table').within(() => cy.contains('button', table.sortField))
+          cy.get('table').within(() =>
+            cy.contains('button', table.sortField).should('have.attr', { 'aria-sort': 'ascending' })
+          )
         })
       })
     })
