9b5faae45ff7708ed70c034ff71e32975ab3e371
NeilLuptonMoJ <neil.lupton@digital.justice.gov.uk>
2022-07-06 12:26:40 +0100

    use wiremock reset  endpoint
    the int tests don't use the /wiremock_mappings, so no need to check for proxy mappings
    Calling the reset endoint removes all mappings/requests
    This removes the need to get all the current mappings and map through them to remove each one

--- mockApis/wiremock.ts
+++ mockApis/wiremock.ts
@@ -12,14 +12,6 @@ export default class Wiremock {
   }
 
   async resetStubs(): Promise<unknown> {
-    const mappingsResponse = await superagent.get(`${this.adminUrl}/mappings`)
-    const body = mappingsResponse.body as { mappings: { id: string; response?: { proxyBaseUrl?: string } }[] }
-
-    const nonProxyMappings = body.mappings.filter(mapping => !mapping?.response?.proxyBaseUrl)
-
-    return Promise.all([
-      Promise.all(nonProxyMappings.map(mapping => superagent.delete(`${this.adminUrl}/mappings/${mapping.id}`))),
-      superagent.delete(`${this.adminUrl}/requests`),
-    ])
+    return superagent.post(`${this.adminUrl}/reset`)
   }
 }
