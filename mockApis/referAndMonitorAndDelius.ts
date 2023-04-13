e60af3ebd830080fb6ff7b36c8fc0f90db644b48
kth13 <kumar.thangavel@gmail.com>
2023-04-13 11:49:17 +0100

    configuring mock API for the new refer and monitor and delius API

---
+++ mockApis/referAndMonitorAndDelius.ts
@@ -0,0 +1,21 @@
+import Wiremock from './wiremock'
+
+export default class ReferAndMonitorAndDeliusMocks {
+  constructor(private readonly wiremock: Wiremock, private readonly mockPrefix: string) {}
+
+  stubSentReferral = async (): Promise<unknown> => {
+    return this.wiremock.stubFor({
+      request: {
+        method: 'PUT',
+        urlPattern: `${this.mockPrefix}/probation-case/([a-zA-Z0-9]*)/referrals`,
+      },
+      response: {
+        status: 200,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        jsonBody: {},
+      },
+    })
+  }
+}
