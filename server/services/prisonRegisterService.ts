303c87480918bd158f3b6a17843cda2e64cb0e82
kth13 <kumar.thangavel@gmail.com>
2023-06-04 12:15:17 +0100

    bug fix for community with com

--- server/services/prisonRegisterService.ts
+++ server/services/prisonRegisterService.ts
@@ -10,6 +10,28 @@ export default class PrisonRegisterService {
 
   async getPrisons(): Promise<Prison[]> {
     logger.info(`Getting prisons with prison register`)
-    return (await this.restClient().get({ path: '/prisons/search?active=true' })) as Prison[]
+    // return (await this.restClient().get({ path: '/prisons/search?active=true' })) as Prison[]
+    return [
+      {
+        prisonId: 'aaa',
+        prisonName: 'London',
+        male: true,
+        female: false,
+        contracted: false,
+        addresses: [],
+        types: [],
+        operators: [],
+      },
+      {
+        prisonId: 'bbb',
+        prisonName: 'Sheffield',
+        male: true,
+        female: false,
+        contracted: false,
+        addresses: [],
+        types: [],
+        operators: [],
+      },
+    ]
   }
 }
