ebde843324ec1d42ae998761fc2cc2ea9c8e74b5
kth13 <kumar.thangavel@gmail.com>
2024-01-31 14:54:44 +0000

    updating the pdu details

--- server/routes/makeAReferral/makeAReferralController.test.ts
+++ server/routes/makeAReferral/makeAReferralController.test.ts
@@ -1245,7 +1245,7 @@ describe('POST /referrals/:id/confirm-probation-practitioner-details', () => {
       {
         ndeliusPPName: 'John Alice',
         ndeliusPPEmailAddress: 'john@example.com',
-        ndeliusPDU: 'Hackney and City',
+        ndeliusPDU: 'Sheffield',
         ndeliusPhoneNumber: '98454243243',
         ndeliusTeamPhoneNumber: '044-2545453442',
       },
