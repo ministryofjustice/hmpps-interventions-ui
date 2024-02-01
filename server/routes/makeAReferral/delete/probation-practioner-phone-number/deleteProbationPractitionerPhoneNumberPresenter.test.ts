a765e7e2c18a4f465f0470da28f969f64b479388
kth13 <kumar.thangavel@gmail.com>
2024-02-01 16:56:01 +0000

    removed unwanted declaration

--- server/routes/makeAReferral/delete/probation-practioner-phone-number/deleteProbationPractitionerPhoneNumberPresenter.test.ts
+++ server/routes/makeAReferral/delete/probation-practioner-phone-number/deleteProbationPractitionerPhoneNumberPresenter.test.ts
@@ -9,28 +9,4 @@ describe(DeleteProbationPractitionerPhoneNumberPresenter, () => {
       expect(presenter.text.label).toEqual('David Blake (CRN: crn)')
     })
   })
-
-  describe('fields', () => {
-    describe('deleteProbationPractitionerPhoneNumber', () => {
-      describe('when probation practitioner phone number have been set', () => {
-        it('uses an empty string value as the field value', () => {
-          const presenter = new DeleteProbationPractitionerPhoneNumberPresenter(
-            '1',
-            'crn',
-            '07534343434',
-            'David',
-            'Blake'
-          )
-
-          expect(presenter.fields.ndeliusPPPhoneNumber).toEqual('07534343434')
-        })
-      })
-      describe('when no probation practitioner phone number have been set', () => {
-        it('uses an empty string value as the field value', () => {
-          const presenter = new DeleteProbationPractitionerPhoneNumberPresenter('1', 'crn', '', 'David', 'Blake')
-          expect(presenter.fields.ndeliusPPPhoneNumber).toEqual('')
-        })
-      })
-    })
-  })
 })
