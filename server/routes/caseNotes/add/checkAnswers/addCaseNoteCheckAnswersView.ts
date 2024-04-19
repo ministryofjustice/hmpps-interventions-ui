206feea63d4d3d093610243d395aed1c2f4b4f0d
kth13 <kumar.thangavel@gmail.com>
2024-04-19 09:15:52 +0100

    remove the email opt in summary for PP

--- server/routes/caseNotes/add/checkAnswers/addCaseNoteCheckAnswersView.ts
+++ server/routes/caseNotes/add/checkAnswers/addCaseNoteCheckAnswersView.ts
@@ -1,4 +1,5 @@
 import { SummaryListArgs } from '../../../../utils/govukFrontendTypes'
+import { SummaryListItem } from '../../../../utils/summaryList'
 import ViewUtils from '../../../../utils/viewUtils'
 import AddCaseNoteCheckAnswersPresenter from './addCaseNoteCheckAnswersPresenter'
 
@@ -11,24 +12,29 @@ export default class AddCaseNoteCheckAnswersView {
   }
 
   private get caseNoteSummaryListArgs(): SummaryListArgs {
-    return ViewUtils.summaryListArgsWithSummaryCard(
-      [
-        { key: 'Subject', lines: [this.presenter.caseNoteSummary.subject] },
-        { key: 'Date', lines: [this.presenter.caseNoteSummary.date] },
-        { key: 'Time', lines: [this.presenter.caseNoteSummary.time] },
-        { key: 'From', lines: [this.presenter.caseNoteSummary.from] },
-        {
-          key: 'Notes about the intervention or the person on probation',
-          lines: [this.presenter.caseNoteSummary.body],
-        },
-        {
-          key: 'Would you like the probation practitioner to get an email about this case note?',
-          lines: [this.presenter.caseNoteSummary.sendEmail],
-        },
-      ],
-      this.presenter.caseNoteDetailsHeading,
-      { showBorders: true, showTitle: true }
+    const caseNotesSummaryListItem: SummaryListItem[] = []
+
+    caseNotesSummaryListItem.push(
+      { key: 'Subject', lines: [this.presenter.caseNoteSummary.subject] },
+      { key: 'Date', lines: [this.presenter.caseNoteSummary.date] },
+      { key: 'Time', lines: [this.presenter.caseNoteSummary.time] },
+      { key: 'From', lines: [this.presenter.caseNoteSummary.from] },
+      {
+        key: 'Notes about the intervention or the person on probation',
+        lines: [this.presenter.caseNoteSummary.body],
+      }
     )
+    if (this.presenter.loggedInUserType === 'service-provider') {
+      caseNotesSummaryListItem.push({
+        key: 'Would you like the probation practitioner to get an email about this case note?',
+        lines: [this.presenter.caseNoteSummary.sendEmail],
+      })
+    }
+
+    return ViewUtils.summaryListArgsWithSummaryCard(caseNotesSummaryListItem, this.presenter.caseNoteDetailsHeading, {
+      showBorders: true,
+      showTitle: true,
+    })
   }
 
   get renderArgs(): [string, Record<string, unknown>] {
