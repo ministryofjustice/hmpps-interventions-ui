5a9721ccb270d192315c698965673399b224e9d0
moj-keith-au <Keith.Au@justice.gov.uk>
2026-01-26 13:12:55 +0000

    Removed downtime banner

--- server/utils/content.ts
+++ server/utils/content.ts
@@ -12,8 +12,11 @@ const content: Content = {
   serviceOutageBanner: {
     title: 'Downtime',
     subHeading: 'Planned Downtime',
-    // to turn off the banner remove or comment out line below
-    text: 'Refer and monitor an intervention will be unavailable between 5pm on Friday 23 January and 8am on Monday 26 January. This is due to planned maintenance in NDelius.',
+    /*
+     * To turn on banner: uncomment and modify the content in below "text: 'content'" line
+     * To turn off the banner: remove or comment out below "text: xxx" line
+     */
+    //text: 'Refer and monitor an intervention will be unavailable between 5pm on Friday 23 January and 8am on Monday 26 January. This is due to planned maintenance in NDelius.',
   },
 }
 export default content
