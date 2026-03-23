e5c17e1143f0fd6f43d5df5bde529bd0be8e0ff7
kumar.thangavel <kumar.thangavel1@justice.gov.uk>
2026-03-23 08:19:28 +0000

    disable downtime banner

--- server/utils/content.ts
+++ server/utils/content.ts
@@ -16,7 +16,7 @@ const content: Content = {
      * To turn on banner: uncomment and modify the content in below "text: 'content'" line
      * To turn off the banner: remove or comment out below "text: xxx" line
      */
-    text: 'Refer and monitor an intervention will be unavailable between 6pm on Friday 20 March and 8am on Monday 23 March. This is due to planned maintenance in NDelius.',
+    //text: 'Refer and monitor an intervention will be unavailable between 6pm on Friday 20 March and 8am on Monday 23 March. This is due to planned maintenance in NDelius.',
   },
 }
 export default content
